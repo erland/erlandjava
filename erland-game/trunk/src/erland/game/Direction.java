f59eb2e305be87a350b6b0dc80618ad8483b89bb
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2001-08-09 21:25:49 +0000

    Flyttad fr�n erland.game.pipes

---
+++ erland-game/trunk/src/erland/game/Direction.java
@@ -0,0 +1,12 @@
+package erland.game;
+
+/**
+ * Specifies all possible directions
+ */
+public interface Direction
+{
+	public static final int LEFT=0;
+	public static final int RIGHT=1;
+	public static final int UP=2;
+	public static final int DOWN=3;
+}
