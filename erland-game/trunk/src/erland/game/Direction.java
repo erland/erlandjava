e450a8d21896b39631b50e12e373ad893ae367a4
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2001-08-12 18:57:12 +0000

    opposite method added

--- erland-game/trunk/src/erland/game/Direction.java
+++ erland-game/trunk/src/erland/game/Direction.java
@@ -3,8 +3,28 @@ package erland.game;
 /**
  * Specifies all possible directions
  */
-public interface Direction
+public abstract class Direction
 {
+	/**
+	 * Returns the opposite direction
+	 * @param direction The direction to return the opposite for
+	 * @return The opposite direction
+	 */
+	public static int opposite(int direction)
+	{
+		switch(direction) {
+			case Direction.LEFT:
+				return Direction.RIGHT;
+			case Direction.RIGHT:
+				return Direction.LEFT;
+			case Direction.UP:
+				return Direction.DOWN;
+			case Direction.DOWN:
+				return Direction.UP;
+			default:
+				return Direction.LEFT;
+		}
+	}
 	public static final int LEFT=0;
 	public static final int RIGHT=1;
 	public static final int UP=2;
