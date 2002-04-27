dba694f38c1c35b7f292a9d657ab32a69bfe5cd9
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2002-04-27 11:23:42 +0000

    Added methods for scrollable container

--- erland-game/trunk/src/erland/game/BlockContainerData.java
+++ erland-game/trunk/src/erland/game/BlockContainerData.java
@@ -32,8 +32,28 @@ public class BlockContainerData
 	 */
 	protected int squareSize;
 
+    /**
+     * Number of visible horizontal blocks
+     */
+    protected int visibleSizeX;
+
+    /**
+     * Number of visible vertical blocks
+     */
+    protected int visibleSizeY;
+
+    /**
+     * Horizontal scrolling offset in pixels
+     */
+    protected int scrollingOffsetX;
+
+    /**
+     * Vertical scrolling offset in pixels
+     */
+    protected int scrollingOffsetY;
+
 	/**
-	 * Creates a new object
+	 * Creates a new object not scrollable
 	 * @param offsetX The X offset in pixels
 	 * @param offsetY The Y offset in pixels
 	 * @param sizeX Number of horizontal blocks
@@ -47,7 +67,34 @@ public class BlockContainerData
 		this.sizeX = sizeX;
 		this.sizeY = sizeY;
 		this.squareSize = squareSize;
+        this.visibleSizeX = sizeX;
+        this.visibleSizeY = sizeY;
+        this.scrollingOffsetX = 0;
+        this.scrollingOffsetY = 0;
 	}
+    /**
+	 * Creates a new object
+	 * @param offsetX The X offset in pixels
+	 * @param offsetY The Y offset in pixels
+	 * @param sizeX Number of horizontal blocks
+	 * @param sizeY Number of vertical blocks
+	 * @param squareSize Size of a single block in pixels
+     * @param visibleSizeX Number of visible horizontal blocks
+     * @param visibleSizeY Number of visible vertical blocks
+	 */
+	public BlockContainerData(int offsetX, int offsetY, int sizeX, int sizeY, int squareSize,int visibleSizeX, int visibleSizeY)
+	{
+		this.offsetX = offsetX;
+		this.offsetY = offsetY;
+		this.sizeX = sizeX;
+		this.sizeY = sizeY;
+		this.squareSize = squareSize;
+        this.visibleSizeX = visibleSizeX;
+        this.visibleSizeY = visibleSizeY;
+        this.scrollingOffsetX = 0;
+        this.scrollingOffsetY = 0;
+	}
+
 	public int getOffsetX()
 	{
 		return offsetX;
@@ -70,11 +117,11 @@ public class BlockContainerData
 	}
     public int getDrawingPositionX(int x)
     {
-    	return getOffsetX() + getSquareSize()*x;
+    	return getOffsetX() - getScrollingOffsetX() + getSquareSize()*x;
     }
     public int getDrawingPositionY(int y)
     {
-    	return getOffsetY() + getSquareSize()*y;
+    	return getOffsetY() - getScrollingOffsetY() + getSquareSize()*y;
     }
     public int getPositionX(int x)
     {
@@ -86,11 +133,20 @@ public class BlockContainerData
     }
     public int getDrawingSizeX()
     {
-    	return getSquareSize()*getSizeX();
+    	return getSquareSize()*visibleSizeX;
     }
     public int getDrawingSizeY()
     {
-    	return getSquareSize()*getSizeY();
+    	return getSquareSize()*visibleSizeY;
+    }
+    public boolean getVisible(int posX, int posY)
+    {
+        if((getPositionX(posX)+getSquareSize())>scrollingOffsetX && getPositionX(posX)<(scrollingOffsetX+getDrawingSizeX())) {
+            if((getPositionY(posY)+getSquareSize())>scrollingOffsetY && getPositionY(posY)<(scrollingOffsetY+getDrawingSizeY())) {
+                return true;
+            }
+        }
+        return false;
     }
 	/**
 	 * Sets the horizontal drawing offset
@@ -108,4 +164,55 @@ public class BlockContainerData
 	{
 		this.offsetY = offsetY;
 	}
+
+    /**
+	 * Sets the horizontal scrolling offset
+	 * @param x The new X scrolling offset
+	 */
+	public void setScrollingOffsetX(int offsetX)
+	{
+		this.scrollingOffsetX = offsetX;
+	}
+	/**
+	 * Sets the horizontal scrolling offset
+	 * @param y The new Y scrolling offset
+	 */
+	public void setScrollingOffsetY(int offsetY)
+	{
+		this.scrollingOffsetY = offsetY;
+	}
+	public int getScrollingOffsetX()
+	{
+		return scrollingOffsetX;
+	}
+
+	public int getScrollingOffsetY()
+	{
+		return scrollingOffsetY;
+	}
+
+    /**
+	 * Get the horizontal scrolling size
+	 * @return The horizontal scrolling offset (Pixel coordinates)
+	 */
+	public int getScrollingSizeX()
+	{
+		return getSizeX()*getSquareSize();
+	}
+	/**
+	 * Get the vertical scrolling size
+	 * @return The vertical scrolling size (Pixel coordinates)
+	 */
+	public int getScrollingSizeY()
+	{
+		return getSizeY()*getSquareSize();
+	}
+
+    public int getPixelDrawingPositionX(int x) {
+        return getOffsetX()-getScrollingOffsetX()+x;
+    }
+
+    public int getPixelDrawingPositionY(int y) {
+        return getOffsetY()-getScrollingOffsetY()+y;
+    }
 }
