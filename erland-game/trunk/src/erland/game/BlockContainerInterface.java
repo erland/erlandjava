0b1755813bb70280677bd986f50836b7de02e6e2
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2002-04-27 11:23:16 +0000

    Added methods for scrollable containers

--- erland-game/trunk/src/erland/game/BlockContainerInterface.java
+++ erland-game/trunk/src/erland/game/BlockContainerInterface.java
@@ -17,12 +17,12 @@ public interface BlockContainerInterface
 	 */
 	int getOffsetY();
 	/**
-	 * Get the number of horizontal blocks
+	 * Get the number of visible horizontal blocks
 	 * @return The number of horizontal blocks
 	 */
 	int getSizeX();
 	/**
-	 * Get the number of vertical blocks
+	 * Get the number of visible vertical blocks
 	 * @return The number of vertical blocks
 	 */
 	int getSizeY();
@@ -32,14 +32,14 @@ public interface BlockContainerInterface
 	 * @return The size of a single block in pixels
 	 */
 	int getSquareSize();
-	
+
 	/**
      * Get the drawing position for a specific block coordinate
      * @param x Block x position (Square coordinate)
      * @return x drawing position in pixels
      */
     int getDrawingPositionX(int x);
-    
+
 	/**
      * Get the drawing position for a specific block coordinate
      * @param y Block y position (Square coordinate)
@@ -47,13 +47,27 @@ public interface BlockContainerInterface
      */
     int getDrawingPositionY(int y);
 
+    /**
+     * Get the drawing position for a specific pixel coordinate
+     * @param x Object x position (Pixel coordinate)
+     * @return x drawing position in pixels
+     */
+    int getPixelDrawingPositionX(int x);
+
+	/**
+     * Get the drawing position for a specific pixel coordinate
+     * @param y Object y position (Pixel coordinate)
+     * @return y drawing position in pixels
+     */
+    int getPixelDrawingPositionY(int y);
+
 	/**
      * Get the position for a specific block coordinate
      * @param x Block x position (Square coordinate)
      * @return x position in pixels
      */
     int getPositionX(int x);
-    
+
 	/**
      * Get the position for a specific block coordinate
      * @param y Block y position (Square coordinate)
@@ -66,10 +80,41 @@ public interface BlockContainerInterface
      * @return x drawing size in pixels
      */
     int getDrawingSizeX();
-    
+
 	/**
      * Get the drawing size for container
      * @return x drawing size in pixels
      */
     int getDrawingSizeY();
+
+    /**
+     * Checks if a specific block is within the visible area
+     * @param posX X position of the block to check (Square coordinate)
+     * @param posY Y position of the block to check (Square coordinate)
+     * @return true/false (Visible/Invisible)
+     */
+    boolean getVisible(int posX, int posY);
+
+    /**
+	 * Get the horizontal scrolling offset
+	 * @return The horizontal scrolling offset
+	 */
+	public int getScrollingOffsetX();
+	/**
+	 * Sets the vertical scrolling offset
+	 * @return The vertical scrolling offset
+	 */
+	public int getScrollingOffsetY();
+
+    /**
+	 * Get the horizontal scrolling size
+	 * @return The horizontal scrolling offset (Pixel coordinates)
+	 */
+	public int getScrollingSizeX();
+
+	/**
+	 * Get the vertical scrolling size
+	 * @return The vertical scrolling size (Pixel coordinates)
+	 */
+	public int getScrollingSizeY();
 }
