e4207ad0f402fe53420cd9b585eb2d3305dc7134
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2003-05-02 19:47:47 +0000

    Updated with access privilliges and comments

--- erland-game/trunk/src/erland/game/FpsCounter.java
+++ erland-game/trunk/src/erland/game/FpsCounter.java
@@ -2,13 +2,26 @@ package erland.game;
 
 import java.awt.*;
 
+/**
+ * Represents a simple class that calculates and draw
+ * a frame rate counter
+ * @author Erland Isaksson
+ */
 public class FpsCounter {
-    String fps;
-    int frameCount;
-    int interval;
-    long lastTime;
+    /** The last calculated frame rate */
+    private String fps;
+    /** Number of frames since last frame rate calculation */
+    private int frameCount;
+    /** Number of frames between calculations */
+    private int interval;
+    /** The time when the last frame rate was calculated */
+    private long lastTime;
 
 
+    /**
+     * Creates a new frame rate calculation object
+     * @param interval Number of frames between frame rate calculations
+     */
     public FpsCounter(int interval)
     {
         this.interval = interval;
@@ -17,6 +30,9 @@ public class FpsCounter {
         fps="??";
     }
 
+    /**
+     * This method should be called each time a new frame is drawn
+     */
     public void update()
     {
         frameCount++;
@@ -28,11 +44,22 @@ public class FpsCounter {
         }
     }
 
+    /**
+     * Draw the last calculated frame rate to the screen
+     * @param g Graphics object to draw on
+     * @param c Color of frame rate counter
+     * @param x X position of the frame rate counter
+     * @param y Y position of the frame rate counter
+     */
     public void draw(Graphics g,Color c, int x, int y)
     {
         Color old = g.getColor();
         g.setColor(c);
         g.drawString(fps,x,y);
         g.setColor(old);
+    }
+
+    public String getFps() {
+        return fps;
     }
 }
