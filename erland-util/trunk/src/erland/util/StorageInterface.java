13f23d8f39215fbde8c8a2d5f7eb9acafca38770
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2003-05-02 19:38:10 +0000

    Added comments

--- erland-util/trunk/src/erland/util/StorageInterface.java
+++ erland-util/trunk/src/erland/util/StorageInterface.java
@@ -1,6 +1,19 @@
 package erland.util;
 
+/**
+ * Defines an inteface for different types of storages that can store a string
+ * @author Erland Isaksson
+ */
 public interface StorageInterface {
+    /**
+     * Store the specified string
+     * @param str String to store
+     */
     public void save(String str);
+
+    /**
+     * Read data from the storage as a string
+     * @return Data in the storage
+     */
     public String load();
 }
