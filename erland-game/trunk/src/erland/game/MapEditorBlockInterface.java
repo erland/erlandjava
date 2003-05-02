446ca8ffb6e6a4527ab24f13ebcadd587f993c97
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2003-05-02 19:50:34 +0000

    Updated with comments

--- erland-game/trunk/src/erland/game/MapEditorBlockInterface.java
+++ erland-game/trunk/src/erland/game/MapEditorBlockInterface.java
@@ -2,10 +2,36 @@ package erland.game;
 
 import erland.util.ParameterSerializable;
 
+/**
+ * This interface must be implemented by all block objects which should
+ * be used together with the {@link MapEditor} class
+ * @author Erland Isaksson
+ */
 public interface MapEditorBlockInterface extends ParameterSerializable {
+    /**
+     * Initialize block
+     * @param environment The game environment the block exists in
+     */
     public void init(GameEnvironmentInterface environment);
+    /**
+     * Set the container that the block exists in
+     * @param cont The container that the block exists in
+     */
     public void setContainer(BlockContainerInterface cont);
+    /**
+     * Get block X position
+     * @return X position of the block
+     */
     public int getPosX();
+    /**
+     * Get block Y position
+     * @return Y position of the block
+     */
     public int getPosY();
+    /**
+     * Set block position
+     * @param x The X position of the block
+     * @param y The Y position of the block
+     */
     public void setPos(int x, int y);
 }
