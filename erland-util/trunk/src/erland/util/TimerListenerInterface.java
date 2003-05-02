2328dc1b3434098171f9ca46936c48f058f646e9
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2003-05-02 19:40:39 +0000

    Updated comments

--- erland-util/trunk/src/erland/util/TimerListenerInterface.java
+++ erland-util/trunk/src/erland/util/TimerListenerInterface.java
@@ -1,11 +1,13 @@
-/*
- * Specifies the interface to implement when you want regular ping calls
- */
 package erland.util;
 
+/**
+ * Specifies the interface to implement when you want regular ping calls
+ * @see Timer
+ * @author Erland Isaksson
+ */
 public interface TimerListenerInterface {
     /*
-     * Timer tick signal
+     * Timer tick signal, will be called when timer exipres
      */
     public void tick();
 }
