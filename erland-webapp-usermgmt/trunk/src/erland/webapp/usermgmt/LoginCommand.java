d7886e33c94aa3030d36d2d2138097257de88e20
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2003-07-25 10:28:51 +0000

    Updated with support for userattribute option

--- erland-webapp-usermgmt/trunk/src/erland/webapp/usermgmt/LoginCommand.java
+++ erland-webapp-usermgmt/trunk/src/erland/webapp/usermgmt/LoginCommand.java
@@ -2,17 +2,23 @@ package erland.webapp.usermgmt;
 
 import erland.webapp.common.CommandInterface;
 import erland.webapp.common.WebAppEnvironmentInterface;
+import erland.webapp.common.CommandOptionsInterface;
 import erland.util.Log;
+import erland.util.ParameterValueStorageExInterface;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-public class LoginCommand implements CommandInterface {
+public class LoginCommand implements CommandInterface, CommandOptionsInterface {
     private WebAppEnvironmentInterface environment;
+    private ParameterValueStorageExInterface options;
     public void init(WebAppEnvironmentInterface environment) {
         this.environment = environment;
     }
 
+    public void setOptions(ParameterValueStorageExInterface options) {
+        this.options = options;
+    }
     public String execute(HttpServletRequest req) {
         HttpSession session = req.getSession(true);
         User user = (User)environment.getEntityFactory().create("user");
@@ -21,7 +27,11 @@ public class LoginCommand implements CommandInterface {
         Log.println(this,"Trying to login as "+user.getUsername()+","+user.getPassword());
         if(user.login(req.getParameter("application"))) {
             Log.println(this,"Successfully login as "+user.getUsername());
-            session.setAttribute("user",user);
+            String userAttribute = options.getParameter("userattribute");
+            if(userAttribute==null) {
+                userAttribute = "user";
+            }
+            session.setAttribute(userAttribute,user);
             return "success";
         }else {
             Log.println(this,"Fail to login as "+user.getUsername());
--- erland-webapp-usermgmt/trunk/src/erland/webapp/usermgmt/LoginStatusCommand.java
+++ erland-webapp-usermgmt/trunk/src/erland/webapp/usermgmt/LoginStatusCommand.java
@@ -2,26 +2,35 @@ package erland.webapp.usermgmt;
 
 import erland.webapp.common.CommandInterface;
 import erland.webapp.common.WebAppEnvironmentInterface;
+import erland.webapp.common.CommandOptionsInterface;
 import erland.util.Log;
+import erland.util.ParameterValueStorageExInterface;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-public class LoginStatusCommand implements CommandInterface {
+public class LoginStatusCommand implements CommandInterface, CommandOptionsInterface {
     private WebAppEnvironmentInterface environment;
+    private ParameterValueStorageExInterface options;
     public void init(WebAppEnvironmentInterface environment) {
         this.environment = environment;
     }
-
+    public void setOptions(ParameterValueStorageExInterface options) {
+        this.options = options;
+    }
     public String execute(HttpServletRequest req) {
         HttpSession session = req.getSession(false);
+        String userAttribute = options.getParameter("userattribute");
+        if(userAttribute==null) {
+            userAttribute = "user";
+        }
         if(session!=null) {
-            User user = (User)session.getAttribute("user");
+            User user = (User)session.getAttribute(userAttribute);
             if(user!=null && user.isValid()) {
                 return "user";
             }
         }
-        if(req.getParameter("user")!=null) {
+        if(req.getParameter(userAttribute)!=null) {
             return "guest";
         }
         return "none";
