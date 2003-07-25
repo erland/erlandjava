8a8779bec3bbd336338f1dbc2c20b2cca30586b2
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2003-07-25 10:29:10 +0000

    Updated with support for userattribute and invalidatesession options

--- erland-webapp-usermgmt/trunk/src/erland/webapp/usermgmt/LogoutCommand.java
+++ erland-webapp-usermgmt/trunk/src/erland/webapp/usermgmt/LogoutCommand.java
@@ -2,18 +2,33 @@ package erland.webapp.usermgmt;
 
 import erland.webapp.common.CommandInterface;
 import erland.webapp.common.WebAppEnvironmentInterface;
+import erland.webapp.common.CommandOptionsInterface;
+import erland.util.ParameterValueStorageExInterface;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-public class LogoutCommand implements CommandInterface {
+public class LogoutCommand implements CommandInterface, CommandOptionsInterface {
+    private WebAppEnvironmentInterface environment;
+    private ParameterValueStorageExInterface options;
     public void init(WebAppEnvironmentInterface environment) {
+        this.environment = environment;
     }
 
+    public void setOptions(ParameterValueStorageExInterface options) {
+        this.options = options;
+    }
     public String execute(HttpServletRequest req) {
         HttpSession session = req.getSession(true);
-        session.removeAttribute("user");
-        session.invalidate();
+        String userAttribute = options.getParameter("userattribute");
+        if(userAttribute==null) {
+            userAttribute = "user";
+        }
+        session.removeAttribute(userAttribute);
+        String invalidateSession = options.getParameter("invalidatesession");
+        if(invalidateSession==null || invalidateSession.equalsIgnoreCase("true")) {
+            session.invalidate();
+        }
         return null;
     }
 }
