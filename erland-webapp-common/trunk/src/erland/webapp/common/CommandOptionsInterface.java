068c396dca70f9aa51e7d0232c28e6b84dcc4dec
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2003-07-25 10:27:30 +0000

    First version stored in cvs

---
+++ erland-webapp-common/trunk/src/erland/webapp/common/CommandOptionsInterface.java
@@ -0,0 +1,7 @@
+package erland.webapp.common;
+
+import erland.util.ParameterValueStorageExInterface;
+
+public interface CommandOptionsInterface {
+    public void setOptions(ParameterValueStorageExInterface options);
+}
