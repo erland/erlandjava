ea67e56e7cd3ddd6959856d161f5d4553ece5d77
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2003-07-25 10:09:42 +0000

    Updated with gallery interface and initCommand template method

--- erland-webapp-gallery/trunk/src/erland/webapp/gallery/gallery/SearchGalleriesCommand.java
+++ erland-webapp-gallery/trunk/src/erland/webapp/gallery/gallery/SearchGalleriesCommand.java
@@ -11,13 +11,14 @@ import javax.servlet.http.HttpServletRequest;
 
 public class SearchGalleriesCommand implements CommandInterface, ViewGalleriesInterface {
     private WebAppEnvironmentInterface environment;
-    private Gallery[] galleries;
+    private GalleryInterface[] galleries;
 
     public void init(WebAppEnvironmentInterface environment) {
         this.environment = environment;
     }
 
     public String execute(HttpServletRequest request) {
+        initCommand(request);
         String username = request.getParameter("user");
         if(username==null || username.length()==0) {
             User user = (User) request.getSession().getAttribute("user");
@@ -25,18 +26,29 @@ public class SearchGalleriesCommand implements CommandInterface, ViewGalleriesIn
         }
         QueryFilter filter = new QueryFilter(getQueryFilter());
         filter.setAttribute("username",username);
-        EntityInterface[] entities = environment.getEntityStorageFactory().getStorage("gallery").search(filter);
-        galleries = new Gallery[entities.length];
+        EntityInterface[] entities = environment.getEntityStorageFactory().getStorage(getEntityName()).search(filter);
+        galleries = new GalleryInterface[entities.length];
         for (int i = 0; i < entities.length; i++) {
-            galleries[i]= (Gallery) entities[i];
+            galleries[i]= (GalleryInterface) entities[i];
         }
         return null;
     }
 
-    public Gallery[] getGalleries() {
+    protected String getEntityName() {
+        return "gallery";
+    }
+
+    public GalleryInterface[] getGalleries() {
         return galleries;
     }
     protected String getQueryFilter() {
         return "allforuser";
+    }
+    protected void initCommand(HttpServletRequest request) {
+        // Do nothing
+    }
+
+    public WebAppEnvironmentInterface getEnvironment() {
+        return environment;
     }
 }
