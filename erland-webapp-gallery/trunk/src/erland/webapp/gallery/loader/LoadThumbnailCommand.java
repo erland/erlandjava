8223fa197b6a2ee37bd776343d951a308830c448
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2003-07-25 10:05:08 +0000

    Updated with copyright overlay support, aspect rate keep support, jpeg compression=none support

--- erland-webapp-gallery/trunk/src/erland/webapp/gallery/loader/LoadThumbnailCommand.java
+++ erland-webapp-gallery/trunk/src/erland/webapp/gallery/loader/LoadThumbnailCommand.java
@@ -4,21 +4,32 @@ import erland.webapp.common.CommandInterface;
 import erland.webapp.common.WebAppEnvironmentInterface;
 import erland.webapp.common.CommandResponseInterface;
 import erland.webapp.gallery.gallery.picture.Picture;
+import erland.webapp.gallery.account.UserAccount;
 import erland.webapp.usermgmt.User;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
+import javax.imageio.IIOImage;
+import javax.imageio.ImageWriteParam;
 import java.io.*;
 import java.awt.image.BufferedImage;
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Iterator;
+
 
 public class LoadThumbnailCommand extends LoadImageCommand implements CommandInterface, CommandResponseInterface {
     private static final int THUMBNAIL_WIDTH=150;
+    private static final int COPYRIGHT_WIDTH=640;
+    private static final int COPYRIGHT_HEIGHT=480;
     private String cacheDir;
+    private UserAccount account;
+    private final int COPYRIGHT_OFFSET = 5;
 
     public void init(WebAppEnvironmentInterface environment) {
         super.init(environment);
@@ -30,10 +41,15 @@ public class LoadThumbnailCommand extends LoadImageCommand implements CommandInt
     }
 
     public void makeResponse(HttpServletRequest request, HttpServletResponse response) {
-        int width = THUMBNAIL_WIDTH;
+        int requestedWidth = THUMBNAIL_WIDTH;
         String widthString = request.getParameter("width");
         if(widthString!=null && widthString.length()>0) {
-            width = Integer.valueOf(widthString).intValue();
+            requestedWidth = Integer.valueOf(widthString).intValue();
+        }
+        Boolean useCache = Boolean.TRUE;
+        String useCacheString = request.getParameter("usecache");
+        if(useCacheString!=null && useCacheString.equalsIgnoreCase("false")) {
+            useCache = Boolean.FALSE;
         }
         String username = request.getParameter("user");
         if(username==null || username.length()==0) {
@@ -42,43 +58,73 @@ public class LoadThumbnailCommand extends LoadImageCommand implements CommandInt
         }
         BufferedImage thumbnail = null;
         try {
-            BufferedInputStream input = null;
-            long lastModified = 0;
-            try {
-                File file = new File(getImageFile());
-                if(file.exists()) {
-                    lastModified = file.lastModified();
-                }
-                input = new BufferedInputStream(new FileInputStream(file));
-            } catch (FileNotFoundException e) {
-                URLConnection connection = new URL(getImageFile()).openConnection();
-                lastModified = connection.getLastModified();
-                input = new BufferedInputStream(connection.getInputStream());
-            }
-            File cachedFile = getFromCache(username,width,getImageFile(),lastModified);
-            if(cachedFile!=null) {
-                write(new FileInputStream(cachedFile),response.getOutputStream());
-            }else {
-                BufferedImage image = ImageIO.read(input);
-
-                if(image!=null) {
-                    int height = width*image.getHeight()/image.getWidth();
-                    thumbnail = new BufferedImage(width,height,image.getType());
-                    Graphics2D g2 = thumbnail.createGraphics();
-                    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-                    g2.drawImage( image, 0, 0, width,height,0,0,image.getWidth(),image.getHeight(),null);
-                    g2.dispose();
+            if(getImageFile()!=null) {
+                BufferedInputStream input = null;
+                long lastModified = 0;
+                try {
+                    File file = new File(getImageFile());
+                    if(file.exists()) {
+                        lastModified = file.lastModified();
+                    }
+                    input = new BufferedInputStream(new FileInputStream(file));
+                } catch (FileNotFoundException e) {
+                    URLConnection connection = new URL(getImageFile()).openConnection();
+                    lastModified = connection.getLastModified();
+                    input = new BufferedInputStream(connection.getInputStream());
                 }
 
-                if(thumbnail!=null) {
-                    response.setContentType("image/jpeg");
-                    ImageIO.write(thumbnail,"JPEG",response.getOutputStream());
-                    setInCache(username,width,getImageFile(),thumbnail);
+                File cachedFile = getFromCache(username,requestedWidth,getImageFile(),lastModified);
+                if(cachedFile!=null) {
+                    write(new FileInputStream(cachedFile),response.getOutputStream());
                 }else {
-                    request.getRequestDispatcher("thumbnailna.gif").forward(request,response);
-                }
-            }
+                    BufferedImage image = ImageIO.read(input);
+                    if(image!=null) {
+                        int width = requestedWidth;
+                        int height = width*image.getHeight()/image.getWidth();
+                        if(((double)image.getWidth()/image.getHeight())<((double)1600/1200)) {
+                            height = width*1200/1600;
+                            width = height*image.getWidth()/image.getHeight();
+                        }
+                        thumbnail = new BufferedImage(width,height,image.getType());
+                        Graphics2D g2 = thumbnail.createGraphics();
+                        g2.drawImage( image, 0, 0, width,height,0,0,image.getWidth(),image.getHeight(),null);
+                        if(width>=COPYRIGHT_WIDTH|| height>=COPYRIGHT_HEIGHT) {
+                            String copyrightText = getCopyrightText();
+                            if(copyrightText!=null && copyrightText.length()>0) {
+                                FontMetrics metrics = g2.getFontMetrics();
+                                Rectangle2D rc = metrics.getStringBounds(copyrightText,g2);
+                                g2.setColor(new Color(1f,1f,1f,0.4f));
+                                g2.fill3DRect(width-(int)rc.getWidth()-COPYRIGHT_OFFSET*3,height-(int)rc.getHeight()-COPYRIGHT_OFFSET*3,(int)rc.getWidth()+COPYRIGHT_OFFSET*2,(int)rc.getHeight()+COPYRIGHT_OFFSET*2,true);
+                                g2.setColor(Color.black);
+                                g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+                                g2.drawString(copyrightText,width-(int)rc.getWidth()-COPYRIGHT_OFFSET*2,height-COPYRIGHT_OFFSET*2-metrics.getDescent());
+                            }
+                        }
+                        g2.dispose();
+                    }
 
+                    if(thumbnail!=null) {
+                        response.setContentType("image/jpeg");
+                        Iterator it = ImageIO.getImageWritersByFormatName("JPEG");
+                        ImageWriter writer = (ImageWriter) it.next();
+                        ImageWriteParam param = writer.getDefaultWriteParam();
+                        param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+                        param.setCompressionQuality(1.0f);
+
+                        writer.setOutput(ImageIO.createImageOutputStream(response.getOutputStream()));
+                        writer.write(null,new IIOImage(thumbnail,null,null),param);
+                        writer.dispose();
+
+                        if(useCache.booleanValue()) {
+                            setInCache(username,requestedWidth,getImageFile(),thumbnail);
+                        }
+                    }else {
+                        request.getRequestDispatcher("thumbnailna.gif").forward(request,response);
+                    }
+                }
+            }else {
+                request.getRequestDispatcher("thumbnailna.gif").forward(request,response);
+            }
         } catch (ServletException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -106,5 +152,21 @@ public class LoadThumbnailCommand extends LoadImageCommand implements CommandInt
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    private UserAccount getUserAccount() {
+        if(account==null) {
+            UserAccount template = (UserAccount) getEnvironment().getEntityFactory().create("useraccount");
+            template.setUsername(getUsername());
+            account = (UserAccount) getEnvironment().getEntityStorageFactory().getStorage("useraccount").load(template);
+        }
+        return account;
+    }
+
+    private String getCopyrightText() {
+        UserAccount account = getUserAccount();
+        if(account!=null) {
+            return account.getCopyrightText();
+        }
+        return null;
     }
 }
