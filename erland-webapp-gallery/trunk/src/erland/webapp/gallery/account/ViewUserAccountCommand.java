8c4751109f3d3eec37e3bf58d92fb47099de0d87
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2003-07-25 09:58:44 +0000

    Updated with GalleryInterface and possiblity to return out parameters from execute method

--- erland-webapp-gallery/trunk/src/erland/webapp/gallery/account/ViewUserAccountCommand.java
+++ erland-webapp-gallery/trunk/src/erland/webapp/gallery/account/ViewUserAccountCommand.java
@@ -5,14 +5,14 @@ import erland.webapp.common.WebAppEnvironmentInterface;
 import erland.webapp.common.QueryFilter;
 import erland.webapp.common.EntityInterface;
 import erland.webapp.usermgmt.User;
-import erland.webapp.gallery.gallery.Gallery;
+import erland.webapp.gallery.gallery.GalleryInterface;
 
 import javax.servlet.http.HttpServletRequest;
 
 public class ViewUserAccountCommand implements CommandInterface, ViewUserAccountInterface {
     private WebAppEnvironmentInterface environment;
     private UserAccount account;
-    private Gallery[] galleries;
+    private GalleryInterface[] galleries;
 
     public void init(WebAppEnvironmentInterface environment) {
         this.environment = environment;
@@ -33,6 +33,15 @@ public class ViewUserAccountCommand implements CommandInterface, ViewUserAccount
         UserAccount template = (UserAccount)environment.getEntityFactory().create("useraccount");
         template.setUsername(username);
         account = (UserAccount) environment.getEntityStorageFactory().getStorage("useraccount").load(template);
+        String differentPages = request.getParameter("withdifferentpages");
+        if(differentPages!=null && differentPages.equals("1")) {
+            if(account!=null && account.getDefaultGallery().intValue()!=0) {
+                request.setAttribute("defaultgallery",account.getDefaultGallery());
+                return "withdefaultgallery";
+            }else {
+                return "withoutdefaultgallery";
+            }
+        }
         return null;
     }
 
@@ -47,14 +56,14 @@ public class ViewUserAccountCommand implements CommandInterface, ViewUserAccount
         return user;
     }
 
-    public Gallery[] getGalleries() {
+    public GalleryInterface[] getGalleries() {
         if(galleries==null) {
             QueryFilter filter = new QueryFilter("allforuser");
             filter.setAttribute("username",account.getUsername());
             EntityInterface[] entities = environment.getEntityStorageFactory().getStorage("gallery").search(filter);
-            galleries = new Gallery[entities.length];
+            galleries = new GalleryInterface[entities.length];
             for (int i = 0; i < entities.length; i++) {
-                galleries[i] = (Gallery) entities[i];
+                galleries[i] = (GalleryInterface) entities[i];
             }
         }
         return galleries;
