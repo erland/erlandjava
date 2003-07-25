5a2c52f7157c809954721320d0224db520266ba5
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2003-07-25 10:10:22 +0000

    Updated with guest user support

--- erland-webapp-gallery/trunk/src/erland/webapp/gallery/gallery/SearchOfficialGalleriesCommand.java
+++ erland-webapp-gallery/trunk/src/erland/webapp/gallery/gallery/SearchOfficialGalleriesCommand.java
@@ -1,7 +1,27 @@
 package erland.webapp.gallery.gallery;
 
+import erland.webapp.usermgmt.User;
+import erland.webapp.gallery.guestaccount.GuestAccountHelper;
+
+import javax.servlet.http.HttpServletRequest;
+
 public class SearchOfficialGalleriesCommand extends SearchGalleriesCommand {
+    private Boolean official;
+    protected void initCommand(HttpServletRequest request) {
+        official=Boolean.TRUE;
+        String username = request.getParameter("user");
+        User user = (User) request.getSession().getAttribute("guestuser");
+        if(user!=null && username!=null) {
+            if(GuestAccountHelper.isGuestUser(getEnvironment(),username,user.getUsername())) {
+                official = Boolean.FALSE;
+            }
+        }
+    }
     protected String getQueryFilter() {
-        return "allofficialforuser";
+        if(official.booleanValue()) {
+            return "allofficialforuser";
+        }else {
+            return super.getQueryFilter();
+        }
     }
 }
