851d6c76f8d3c1b921833b6929377dde1513f1fd
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2003-08-01 21:51:23 +0000

    First version stored in cvs

---
+++ erland-webapp-gallery/trunk/database/gallery_data.sql
@@ -0,0 +1,32 @@
+-- MySQL dump 9.08
+--
+-- Host: localhost    Database: gallery
+---------------------------------------------------------
+-- Server version	4.0.13-max-nt
+
+--
+-- Dumping data for table 'descriptiontags'
+--
+
+INSERT INTO descriptiontags VALUES ('metadatafielddescription','Exif+Exif+Image+Height','Höjd');
+INSERT INTO descriptiontags VALUES ('metadatafielddescription','Exif+Exif+Image+Width','Bredd');
+INSERT INTO descriptiontags VALUES ('metadatafielddescription','Exif+F-Number','Bländare');
+INSERT INTO descriptiontags VALUES ('metadatafielddescription','Exif+Flash','Blixt');
+INSERT INTO descriptiontags VALUES ('metadatafielddescription','Exif+Exposure+Time','Exponeringstid');
+INSERT INTO descriptiontags VALUES ('metadatafielddescription','Exif+Focal+Length','Brännvidd');
+INSERT INTO descriptiontags VALUES ('metadatafielddescription','Exif+ISO+Speed+Ratings','ISO');
+INSERT INTO descriptiontags VALUES ('metadatafielddescription','Exif+Date/Time','Datum/Tid');
+
+--
+-- Dumping data for table 'useraccounts'
+--
+
+INSERT INTO useraccounts VALUES ('test','You+can+change+this+text+and+a+lot+more+by+selecting+Preferences+in+the+menu+to+the+left.%0D%0AWhen+you+feel+that+you+have+entered+all+neccesary+information+you+can+make+your+gallery+visible+on+the+front+page+by+marking+it+as+official+in+the+preferences','','',0,'',0);
+
+
+--
+-- Dumping data for table 'resources'
+--
+
+INSERT INTO resources VALUES ('resources.thumbnail.cache','D%3A%5Cusers%5Cerland%5Cthumbnails');
+
