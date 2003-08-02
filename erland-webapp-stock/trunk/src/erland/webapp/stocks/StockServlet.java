1a1a861b47b107ff46eedd5bb587129ebe03f134
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2003-08-02 06:29:34 +0000

    Disabled some logging

--- erland-webapp-stock/trunk/src/erland/webapp/stocks/StockServlet.java
+++ erland-webapp-stock/trunk/src/erland/webapp/stocks/StockServlet.java
@@ -36,14 +36,14 @@ public class StockServlet extends UserMgmtServlet {
                 "<log>"+
                 "<logitem1>erland.webapp.usermgmt.UserMgmtServlet</logitem1>"+
                 "<logitem2>erland.webapp.stocks.StockServlet</logitem2>"+
-                "<logitem3>erland.webapp.stocks.sb.SBXMLEncoder</logitem3>"+
-                "<logitem4>erland.webapp.stocks.sb.SBXMLParser</logitem4>"+
-                "<logitem5>erland.webapp.stocks.Stock</logitem5>"+
-                "<logitem6>erland.webapp.stocks.StockAccount</logitem6>"+
-                "<logitem7>erland.webapp.stocks.StockAccountCommand</logitem7>"+
-                "<logitem8>erland.webapp.common.BaseServlet</logitem8>"+
-                "<logitem9>erland.webapp.diagram.DateValueDiagram</logitem9>"+
-                "<logitem10>erland.webapp.common.EntityStorage</logitem10>"+
+                "<logitem3>erland.webapp.stocks.sb.SBXMLEncoderDisabled</logitem3>"+
+                "<logitem4>erland.webapp.stocks.sb.SBXMLParserDisabled</logitem4>"+
+                "<logitem5>erland.webapp.stocks.StockDisabled</logitem5>"+
+                "<logitem6>erland.webapp.stocks.StockAccountDisabled</logitem6>"+
+                "<logitem7>erland.webapp.stocks.StockAccountCommandDisabled</logitem7>"+
+                "<logitem8>erland.webapp.common.BaseServletDisabled</logitem8>"+
+                "<logitem9>erland.webapp.diagram.DateValueDiagramDisabled</logitem9>"+
+                "<logitem10>erland.webapp.common.EntityStorageDisabled</logitem10>"+
                 "</log>"),null,"log"));
 
     }
