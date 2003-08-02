0e97d35a653232539fb2822a462ffeeb9f6ebc41
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2003-08-02 08:26:33 +0000

    no message

--- erland-game-racer/trunk/src/erland/game/racer/RacerModelNetworked.java
+++ erland-game-racer/trunk/src/erland/game/racer/RacerModelNetworked.java
@@ -1,11 +1,22 @@
-/**
- * Created by IntelliJ IDEA.
- * User: Erland Isaksson
- * Date: Jan 3, 2003
- * Time: 2:25:49 PM
- * To change this template use Options | File Templates.
- */
 package erland.game.racer;
+/*
+ * Copyright (C) 2003 Erland Isaksson (erland_i@hotmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
 
 import erland.game.*;
 import erland.network.NetworkConnectionInterface;
