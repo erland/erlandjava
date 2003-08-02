3b211850616946cd12b68e1dbdcbdf48529294b3
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2003-08-02 08:09:06 +0000

    no message

--- erland-game-pipes/trunk/src/erland/game/pipes/Pipes.java
+++ erland-game-pipes/trunk/src/erland/game/pipes/Pipes.java
@@ -1,11 +1,22 @@
-/**
- * Created by IntelliJ IDEA.
- * User: Erland Isaksson
- * Date: Jan 2, 2003
- * Time: 3:25:13 PM
- * To change this template use Options | File Templates.
- */
 package erland.game.pipes;
+/*
+ * Copyright (C) 2003 Erland Isaksson (erland_i@hotmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
 
 import erland.game.GamePanelHandlerApplicationInterface;
 import erland.game.GamePanelHandlerForApplication;
