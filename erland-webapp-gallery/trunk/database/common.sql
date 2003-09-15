13b421b1073bc1c6c247f1adac763505f6c547c5
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2003-09-15 17:52:11 +0000

    First version in cvs

---
+++ erland-webapp-gallery/trunk/database/common.sql
@@ -0,0 +1,28 @@
+-- MySQL dump 9.08
+--
+-- Host: localhost    Database: common
+---------------------------------------------------------
+-- Server version	4.0.13-max-nt
+
+--
+-- Table structure for table 'descriptiontags'
+--
+
+CREATE TABLE descriptiontags (
+  type varchar(100) NOT NULL default '',
+  tag varchar(100) NOT NULL default '',
+  description varchar(100) NOT NULL default '',
+  PRIMARY KEY  (type,tag)
+) TYPE=MyISAM;
+
+--
+-- Table structure for table 'resources'
+--
+
+CREATE TABLE resources (
+  application varchar(100) NOT NULL default '',
+  id varchar(100) NOT NULL default '',
+  value varchar(255) NOT NULL default '',
+  PRIMARY KEY  (application,id)
+) TYPE=MyISAM;
+
