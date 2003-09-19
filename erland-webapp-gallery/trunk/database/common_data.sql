245139b2f2dbbf5e3cf0ff12be68ff903baf6ca1
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2003-09-19 05:23:21 +0000

    Updated welcometext so it open links in same window

--- erland-webapp-gallery/trunk/database/common_data.sql
+++ erland-webapp-gallery/trunk/database/common_data.sql
@@ -18,7 +18,7 @@ INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+Expo
 INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+Focal+Length','Brännvidd');
 INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+ISO+Speed+Ratings','ISO');
 INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+Date/Time','Datum/Tid');
-INSERT INTO descriptiontags VALUES ('gallery-description','welcometext','Logga in för att uppdatera ditt bildarkiv eller klicka [boldlink=portal?do=home&user=erland,här] för att titta i mitt bildarkiv. Du kan också titta på någon annans bildarkiv genom att klicka på länkarna nedan.\r\nDu kan också registrera ditt eget bildarkiv [boldlink=portal?do=registernewuseraccount,här]\r\n\r\n');
+INSERT INTO descriptiontags VALUES ('gallery-description','welcometext','Logga in för att uppdatera ditt bildarkiv eller klicka [boldlinksamewindow=http://erland.homeip.net/gallery/portal?do=home&user=erland,här] för att titta i mitt bildarkiv. Du kan också titta på någon annans bildarkiv genom att klicka på länkarna nedan.\r\nDu kan också registrera ditt eget bildarkiv [boldlinksamewindow=portal?do=registernewuseraccount,här]\r\n\r\n');
 
 --
 -- Dumping data for table 'resources'
