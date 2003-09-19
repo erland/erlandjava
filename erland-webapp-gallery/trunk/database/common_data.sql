cf678cb074ec50dde4981e39aabbac98aeb029ae
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2003-09-19 05:06:24 +0000

    Added entry for welcometext in decriptiontags

--- erland-webapp-gallery/trunk/database/common_data.sql
+++ erland-webapp-gallery/trunk/database/common_data.sql
@@ -18,6 +18,7 @@ INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+Expo
 INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+Focal+Length','Brännvidd');
 INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+ISO+Speed+Ratings','ISO');
 INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+Date/Time','Datum/Tid');
+INSERT INTO descriptiontags VALUES ('gallery-description','welcometext','Logga in för att uppdatera ditt bildarkiv eller klicka [boldlink=portal?do=home&user=erland,här] för att titta i mitt bildarkiv. Du kan också titta på någon annans bildarkiv genom att klicka på länkarna nedan.\r\nDu kan också registrera ditt eget bildarkiv [boldlink=portal?do=registernewuseraccount,här]\r\n\r\n');
 
 --
 -- Dumping data for table 'resources'
