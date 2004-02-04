cbc903982ac976a389e25bc5314ddc07f38453f5
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2004-02-04 16:29:13 +0000

    Updated with handlins so it is possible to show all metadata and to just show selected metadata

--- erland-webapp-gallery/trunk/src/erland/webapp/gallery/act/loader/LoadMetadataAction.java
+++ erland-webapp-gallery/trunk/src/erland/webapp/gallery/act/loader/LoadMetadataAction.java
@@ -21,42 +21,50 @@ package erland.webapp.gallery.act.loader;
 
 import erland.webapp.common.EntityInterface;
 import erland.webapp.common.QueryFilter;
+import erland.webapp.common.ServletParameterHelper;
 import erland.webapp.common.act.BaseAction;
 import erland.webapp.common.image.JPEGMetadataHandler;
 import erland.webapp.common.image.MetadataHandlerInterface;
 import erland.webapp.gallery.act.gallery.GalleryHelper;
 import erland.webapp.gallery.entity.gallery.Gallery;
+import erland.webapp.gallery.entity.gallery.GalleryInterface;
 import erland.webapp.gallery.entity.gallery.picture.Picture;
+import erland.webapp.gallery.entity.gallery.picture.Resolution;
 import erland.webapp.gallery.entity.gallery.picturestorage.PictureStorage;
 import erland.webapp.gallery.fb.loader.MetadataImageFB;
 import erland.webapp.gallery.fb.loader.MetadataPB;
+import erland.webapp.gallery.fb.loader.MetadataCollectionPB;
+import erland.webapp.gallery.fb.gallery.picture.ResolutionPB;
+import erland.webapp.gallery.fb.gallery.picture.PicturePB;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionForward;
+import org.apache.commons.beanutils.PropertyUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoadMetadataAction extends BaseAction {
     protected void executeLogic(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
         MetadataImageFB fb = (MetadataImageFB) form;
-        Integer gallery = GalleryHelper.getGalleryId(getEnvironment(), fb.getGallery());
+        GalleryInterface gallery = GalleryHelper.getGallery(getEnvironment(),fb.getGallery());
+        Integer galleryId = GalleryHelper.getGalleryId(gallery);
         Picture template = (Picture) getEnvironment().getEntityFactory().create("gallery-picture");
-        template.setGallery(gallery);
+        template.setGallery(galleryId);
         template.setId(fb.getImage());
         Picture picture = (Picture) getEnvironment().getEntityStorageFactory().getStorage("gallery-picture").load(template);
         if (picture == null) {
             saveErrors(request, Arrays.asList(new String[]{"gallery.loader.picture-dont-exist"}));
             return;
         }
-        Gallery templateGallery = (Gallery) getEnvironment().getEntityFactory().create("gallery-gallery");
-        templateGallery.setId(gallery);
-        Gallery entity = (Gallery) getEnvironment().getEntityStorageFactory().getStorage("gallery-gallery").load(templateGallery);
-        if (entity == null) {
+        if (gallery == null) {
             saveErrors(request, Arrays.asList(new String[]{"gallery.loader.gallery-dont-exist"}));
             return;
         }
-        String username = entity.getUsername();
+        String username = gallery.getUsername();
         QueryFilter filter = new QueryFilter("allforuser");
         filter.setAttribute("username", username);
         EntityInterface[] storageEntities = getEnvironment().getEntityStorageFactory().getStorage("gallery-picturestorage").search(filter);
@@ -72,14 +80,50 @@ public class LoadMetadataAction extends BaseAction {
         MetadataHandlerInterface handler = new JPEGMetadataHandler(!fb.getShowAll().booleanValue());
         handler.load(imageFile);
         String[] names = handler.getNames();
-        MetadataPB[] pb = new MetadataPB[names.length];
-        for (int i = 0; i < pb.length; i++) {
-            pb[i] = new MetadataPB();
-            pb[i].setId(names[i]);
-            pb[i].setDescription(handler.getDescription(names[i]));
-            pb[i].setValue(handler.getValue(names[i]));
+        MetadataPB[] metadata = new MetadataPB[names.length];
+        for (int i = 0; i < metadata.length; i++) {
+            metadata[i] = new MetadataPB();
+            metadata[i].setId(names[i]);
+            metadata[i].setDescription(handler.getDescription(names[i]));
+            metadata[i].setValue(handler.getValue(names[i]));
+        }
+        MetadataCollectionPB pb = new MetadataCollectionPB();
+        pb.setItems(metadata);
+        pb.setShowAll(fb.getShowAll());
+
+        Map parameters = new HashMap();
+        parameters.put("gallery",fb.getGallery());
+        parameters.put("picture",fb.getImage());
+        if(fb.getWidth()!=null) {
+            parameters.put("width",fb.getWidth());
+        }
+        ActionForward forward = mapping.findForward("show-all-metadata-link");
+        if(forward!=null) {
+            pb.setShowAllLink(ServletParameterHelper.replaceDynamicParameters(forward.getPath(),parameters));
+        }
+        forward = mapping.findForward("show-selected-metadata-link");
+        if(forward!=null) {
+            pb.setShowSelectedLink(ServletParameterHelper.replaceDynamicParameters(forward.getPath(),parameters));
         }
         request.setAttribute("metadataPB", pb);
+
+        Map pictureParameters = new HashMap();
+        PicturePB picturePB = new PicturePB();
+        PropertyUtils.copyProperties(picturePB, picture);
+        pictureParameters.put("gallery",fb.getGallery());
+        pictureParameters.put("picture",picture.getId());
+        if(fb.getWidth()!=null) {
+            pictureParameters.put("width",fb.getWidth());
+        }
+
+        if (picturePB.getImage().startsWith("{")) {
+            String path = mapping.findForward("picture-image").getPath();
+            picturePB.setImage(ServletParameterHelper.replaceDynamicParameters(path,pictureParameters));
+        } else {
+            picturePB.setImage(imageFile);
+        }
+        picturePB.setGallery(fb.getGallery());
+        request.setAttribute("picturePB",picturePB);
     }
 
     protected String getImageFileName(Picture picture) {
