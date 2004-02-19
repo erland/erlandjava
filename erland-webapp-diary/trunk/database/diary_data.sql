00fcebefd18ff83960305c694c15563a13ad75c1
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2004-02-19 20:08:31 +0000

    Added inventoryentrytypes, inventoryentryeventtypes and removed resources

--- erland-webapp-diary/trunk/database/diary_data.sql
+++ erland-webapp-diary/trunk/database/diary_data.sql
@@ -1,20 +1,31 @@
--- MySQL dump 9.08
+-- MySQL dump 9.09
 --
 -- Host: localhost    Database: diary
 ---------------------------------------------------------
--- Server version	4.0.13-max-nt
+-- Server version	4.0.15-max-debug
 
 
 --
--- Dumping data for table 'useraccounts'
+-- Dumping data for table `inventoryentryeventtypes`
 --
 
-INSERT INTO useraccounts VALUES ('test','','','',0,0);
-
+INSERT INTO inventoryentryeventtypes VALUES (1,'Ink%C3%B6pt');
+INSERT INTO inventoryentryeventtypes VALUES (2,'D%C3%B6dad');
+INSERT INTO inventoryentryeventtypes VALUES (3,'D%C3%B6d');
+INSERT INTO inventoryentryeventtypes VALUES (4,'S%C3%A5ld');
+INSERT INTO inventoryentryeventtypes VALUES (5,'Lekt');
+INSERT INTO inventoryentryeventtypes VALUES (6,'Yngel');
+INSERT INTO inventoryentryeventtypes VALUES (7,'Uppm%C3%A4tt');
 
 --
--- Dumping data for table 'resources'
+-- Dumping data for table `inventoryentrytypes`
 --
 
-INSERT INTO resources VALUES ('resources.thumbnail.cache','D%3A%5Cusers%5Cerland%5Cthumbnails');
+INSERT INTO inventoryentrytypes VALUES (1,'Fisk');
+
+--
+-- Dumping data for table `useraccounts`
+--
+
+INSERT INTO useraccounts VALUES ('test','Du+kan+%C3%A4ndra+denna+text+och+mycket+annat+genom+att+v%C3%A4lja+Inst%C3%A4llningar+i+menyn+till+v%C3%A4nster.%0D%0AN%C3%A4r+du+har+registrerat+all+n%C3%B6dv%C3%A4ndig+information+i+ditt+bildarkiv+can+du+g%C3%B6ra+det+synligt+p%C3%A5+f%C3%B6rstasidan+genom+att+s%C3%A4tta+en+markering+i+Inst%C3%A4llningar','En+kort+beskrivning+av+detta+bild+arkiv','',0,0);
 
