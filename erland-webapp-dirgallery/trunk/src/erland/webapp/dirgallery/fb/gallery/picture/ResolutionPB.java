07a588e0b4490097d1f43439fe0494564abd21ad
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2004-02-25 06:53:59 +0000

    Moved, renamed or added updated due to struts adaption

---
+++ erland-webapp-dirgallery/trunk/src/erland/webapp/dirgallery/fb/gallery/picture/ResolutionPB.java
@@ -0,0 +1,73 @@
+package erland.webapp.dirgallery.fb.gallery.picture;
+
+import erland.webapp.common.fb.BasePB;
+
+/*
+ * Copyright (C) 2004 Erland Isaksson (erland_i@hotmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ */
+
+public class ResolutionPB extends BasePB {
+    private String id;
+    private String description;
+    private Integer width;
+
+    public ResolutionPB() {
+    }
+
+    public ResolutionPB(String id, String description, Integer width) {
+        this.id = id;
+        this.description = description;
+        this.width = width;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public String getWidthDisplay() {
+        return width != null ? width.toString() : null;
+    }
+
+    public void setWidthDisplay(String widthDisplay) {
+        try {
+            this.width = Integer.valueOf(widthDisplay);
+        } catch (NumberFormatException e) {
+            this.width = null;
+        }
+    }
+}
---
+++ erland-webapp-dirgallery/trunk/web/WEB-INF/tiles/gallery/editcomment.jsp
@@ -0,0 +1,19 @@
+<%@ taglib uri="http://jakarta.apache.org/struts/tags-bean" prefix="bean" %>
+<%@ taglib uri="http://jakarta.apache.org/struts/tags-html" prefix="html" %>
+<%@ taglib uri="http://jakarta.apache.org/struts/tags-tiles" prefix="tiles" %>
+<%@ taglib uri="http://jakarta.apache.org/struts/tags-logic" prefix="logic" %>
+<%@ taglib uri="http://erland.homeip.net/tags/erland-common" prefix="erland-common" %>
+
+<jsp:include page="/WEB-INF/tiles/common/validationerrors.jsp" />
+<html:form action="/user/editimagecomment" method="POST">
+    <table>
+    <html:hidden property="id"/>
+    <html:hidden property="gallery"/>
+    <tr><td><bean:message key="dirgallery.gallery.comment.edit.comment"/></td><td>
+    <html:textarea property="comment" cols="60" rows="5"/>
+    </td></tr>
+    <tr><td></td><td>
+    <html:submit><bean:message key="dirgallery.buttons.save"/></html:submit>
+    </td></tr>
+    <table>
+</html:form>
