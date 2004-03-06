448caee87e21d7c2d44542f6593d2f82f1d4a2d3
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2004-03-06 10:26:01 +0000

    Updated so it inherits MenuExpandAction

--- erland-webapp-common/trunk/src/erland/webapp/common/act/MenuExpandCollapseAction.java
+++ erland-webapp-common/trunk/src/erland/webapp/common/act/MenuExpandCollapseAction.java
@@ -30,36 +30,7 @@ import java.util.Arrays;
  * 
  */
 
-public class MenuExpandCollapseAction extends BaseAction {
-    protected void preProcess(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
-        MenuExpandCollapseFB fb = (MenuExpandCollapseFB) form;
-        if(getConfigurableParameter("menuName",fb.getMenuName(),mapping.getParameter(),request)==null) {
-            saveErrors(request,Arrays.asList(new String[] {"errors.invalid-parameter","menuName"}));
-        }
-        if(getConfigurableParameter("menuItemId",fb.getMenuItemId(),mapping.getParameter(),request)==null) {
-            saveErrors(request,Arrays.asList(new String[] {"errors.invalid-parameter","menuItemId"}));
-        }
-    }
-
-    protected String getConfigurableParameter(String name, String value, String parameterString, HttpServletRequest request) {
-        if(parameterString!=null) {
-            String parameterName =ServletParameterHelper.getParameter(parameterString,name+"Parameter");
-            if(parameterName!=null&&parameterName.length()>0) {
-                value = request.getParameter(parameterName);
-            }
-            if(value==null) {
-                value = ServletParameterHelper.getParameter(parameterString,name+"Prefix");
-            }else {
-                String prefix = ServletParameterHelper.getParameter(parameterString,name+"Prefix");
-                value = (prefix!=null?prefix:"")+value;
-            }
-            if(value==null) {
-                value = ServletParameterHelper.getParameter(parameterString,name);
-            }
-        }
-        return value;
-    }
-
+public class MenuExpandCollapseAction extends MenuExpandAction {
     protected void executeLogic(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
         MenuExpandCollapseFB fb = (MenuExpandCollapseFB) form;
         String menuName = getConfigurableParameter("menuName",fb.getMenuName(),mapping.getParameter(),request);
@@ -77,29 +48,5 @@ public class MenuExpandCollapseAction extends BaseAction {
         }else {
             request.getSession().setAttribute(menuName,menuItemId);
         }
-    }
-
-    protected ActionForward findSuccess(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
-        MenuExpandCollapseFB fb = (MenuExpandCollapseFB) form;
-        String menuName = getConfigurableParameter("menuName",fb.getMenuName(),mapping.getParameter(),request);
-        String menuItemId = getConfigurableParameter("menuItemId",fb.getMenuItemId(),mapping.getParameter(),request);
-        String id=menuItemId;
-        ActionForward forward = mapping.findForward(menuName+"-"+id);
-        while(forward==null && id.length()>0) {
-            int pos = id.lastIndexOf("-");
-            if(pos>0) {
-                id = id.substring(0,pos);
-                forward = mapping.findForward(menuName+"-"+id);
-            }else {
-                id="";
-            }
-        }
-        if(forward==null) {
-            forward = mapping.findForward(menuName);
-        }
-        if(forward==null) {
-            forward = mapping.findForward(FORWARD_SUCCESS);
-        }
-        return forward;
     }
 }
