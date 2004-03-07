e745b1731ea91f93fda81851a2ec5b21ac83b50a
nobody <nobody@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2004-03-07 14:18:29 +0000

    This commit was manufactured by cvs2svn to create tag 'rel_1_5'.

---
+++ erland-webapp-common/tags/rel_1_5/src/erland/webapp/common/ResourceStorage.java
@@ -0,0 +1,104 @@
+package erland.webapp.common;
+/*
+ * Copyright (C) 2003 Erland Isaksson (erland_i@hotmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+import erland.util.ParameterValueStorageExInterface;
+import erland.util.StorageInterface;
+import erland.util.StringStorage;
+import erland.webapp.common.WebAppEnvironmentInterface;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class ResourceStorage implements ParameterValueStorageExInterface {
+    private String entityName;
+    private WebAppEnvironmentInterface environment;
+    private boolean enableCache;
+    private Map cache = new HashMap();
+    private String application;
+
+    public ResourceStorage(WebAppEnvironmentInterface environment, String application, String entityName, boolean enableCache) {
+        this.entityName = entityName;
+        this.environment = environment;
+        this.enableCache = enableCache;
+        this.application = application;
+    }
+    public StorageInterface getParameterAsStorage(String name) {
+        if(enableCache) {
+            String value = (String) cache.get(name);
+            if(value!=null) {
+                return new StringStorage("<"+name+">"+value+"</"+name+">");
+            }
+        }
+        ResourceInterface resource = (ResourceInterface) environment.getEntityFactory().create(entityName);
+        resource.setApplication(application);
+        resource.setId(name);
+        resource = (ResourceInterface) environment.getEntityStorageFactory().getStorage(entityName).load(resource);
+        if(resource!=null) {
+            if(enableCache) {
+                cache.put(name,resource.getValue());
+            }
+            return new StringStorage("<"+name+">"+resource.getValue()+"</"+name+">");
+        }else {
+            return null;
+        }
+    }
+
+    public String getParameter(String name) {
+        if(enableCache) {
+            String value = (String) cache.get(name);
+            if(value!=null) {
+                return value;
+            }
+        }
+        ResourceInterface resource = (ResourceInterface) environment.getEntityFactory().create(entityName);
+        resource.setApplication(application);
+        resource.setId(name);
+        resource = (ResourceInterface) environment.getEntityStorageFactory().getStorage(entityName).load(resource);
+        if(resource!=null) {
+            if(enableCache) {
+                cache.put(name,resource.getValue());
+            }
+            return resource.getValue();
+        }else {
+            return null;
+        }
+    }
+
+    public void setParameter(String name, String value) {
+        ResourceInterface resource = (ResourceInterface) environment.getEntityFactory().create(entityName);
+        resource.setApplication(application);
+        resource.setId(name);
+        resource.setValue(value);
+        environment.getEntityStorageFactory().getStorage(entityName).store(resource);
+        if(enableCache) {
+            cache.put(name,value);
+        }
+    }
+
+    public void delParameter(String name) {
+        ResourceInterface resource = (ResourceInterface) environment.getEntityFactory().create(entityName);
+        resource.setApplication(application);
+        resource.setId(name);
+        environment.getEntityStorageFactory().getStorage(entityName).delete(resource);
+        if(enableCache) {
+            cache.remove(name);
+        }
+    }
+}
---
+++ erland-webapp-common/tags/rel_1_5/src/erland/webapp/common/tags/tablegrid/TableGridTag.java
@@ -0,0 +1,417 @@
+package erland.webapp.common.tags.tablegrid;
+
+import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.struts.util.ResponseUtils;
+
+import javax.servlet.jsp.tagext.TagSupport;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Collection;
+import java.io.IOException;
+
+import erland.webapp.common.ServletParameterHelper;
+import erland.util.Log;
+import erland.util.StringUtil;
+
+/*
+ * Copyright (C) 2004 Erland Isaksson (erland_i@hotmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ */
+
+public class TableGridTag extends TagSupport {
+    private String rows;
+    private String cols;
+    private String nameRowsCols;
+    private String rowsProperty;
+    private String colsProperty;
+    private String tableStyle;
+    private String rowIterations;
+    private String cellStyle;
+    private String width;
+    private String height;
+    private String cellWidth;
+    private String cellHeight;
+    private String align;
+    private String valign;
+    private String name;
+    private String property;
+    private String id;
+    private String indexId;
+
+    private Object[] iterateObject;
+    private int index;
+    private int rowIndex;
+    private int noOfRows;
+    private int noOfCols;
+    private int noOfRowIterations;
+
+
+    public String getRows() {
+        return rows;
+    }
+
+    public void setRows(String rows) {
+        this.rows = rows;
+    }
+
+    public String getCols() {
+        return cols;
+    }
+
+    public void setCols(String cols) {
+        this.cols = cols;
+    }
+
+    public String getNameRowsCols() {
+        return nameRowsCols;
+    }
+
+    public void setNameRowsCols(String nameRowsCols) {
+        this.nameRowsCols = nameRowsCols;
+    }
+
+    public String getRowsProperty() {
+        return rowsProperty;
+    }
+
+    public void setRowsProperty(String rowsProperty) {
+        this.rowsProperty = rowsProperty;
+    }
+
+    public String getColsProperty() {
+        return colsProperty;
+    }
+
+    public void setColsProperty(String colsProperty) {
+        this.colsProperty = colsProperty;
+    }
+
+    public String getTableStyle() {
+        return tableStyle;
+    }
+
+    public void setTableStyle(String tableStyle) {
+        this.tableStyle = tableStyle;
+    }
+
+    public String getRowIterations() {
+        return rowIterations;
+    }
+
+    public void setRowIterations(String rowIterations) {
+        this.rowIterations = rowIterations;
+    }
+
+    public String getCellStyle() {
+        return cellStyle;
+    }
+
+    public void setCellStyle(String cellStyle) {
+        this.cellStyle = cellStyle;
+    }
+
+    public String getWidth() {
+        return width;
+    }
+
+    public void setWidth(String width) {
+        this.width = width;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getCellWidth() {
+        return cellWidth;
+    }
+
+    public void setCellWidth(String cellWidth) {
+        this.cellWidth = cellWidth;
+    }
+
+    public String getCellHeight() {
+        return cellHeight;
+    }
+
+    public void setCellHeight(String cellHeight) {
+        this.cellHeight = cellHeight;
+    }
+
+    public String getAlign() {
+        return align;
+    }
+
+    public void setAlign(String align) {
+        this.align = align;
+    }
+
+    public String getValign() {
+        return valign;
+    }
+
+    public void setValign(String valign) {
+        this.valign = valign;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProperty() {
+        return property;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIndexId() {
+        return indexId;
+    }
+
+    public void setIndexId(String indexId) {
+        this.indexId = indexId;
+    }
+
+    public int getRowIndex() {
+        return rowIndex;
+    }
+
+    public int doStartTag() throws JspException {
+        try {
+            JspWriter out = pageContext.getOut();
+            if(Log.isEnabled(this,Log.DEBUG)) {
+                Log.println(this,StringUtil.beanToString(this,null,TagSupport.class,true));
+            }
+            Object bean = (Object) pageContext.findAttribute(name);
+            if(bean!=null && property!=null) {
+                Object beanProperty = null;
+                try {
+                    beanProperty = (Object) PropertyUtils.getProperty(bean,property);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                } catch (NoSuchMethodException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+                if(beanProperty!=null) {
+                    bean = beanProperty;
+                }
+            }
+            String rows = this.rows;
+            String cols = this.cols;
+            if((rows==null || cols==null) && nameRowsCols!=null) {
+                Object rowsColsBean = (Object) pageContext.findAttribute(nameRowsCols);
+                if(rowsColsBean!=null && rows==null && rowsProperty!=null) {
+                    try {
+                        Object rowsValue = (Object) PropertyUtils.getProperty(rowsColsBean,rowsProperty);
+                        if(rowsValue!=null) {
+                            rows = rowsValue.toString();
+                        }
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                    } catch (NoSuchMethodException e) {
+                        e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                    }
+                }
+                if(rowsColsBean!=null && cols==null && colsProperty!=null) {
+                    try {
+                        Object colsValue = (Object) PropertyUtils.getProperty(rowsColsBean,colsProperty);
+                        if(colsValue!=null) {
+                            cols = colsValue.toString();
+                        }
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                    } catch (NoSuchMethodException e) {
+                        e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                    }
+                }
+            }
+
+            if(bean instanceof Object[]) {
+                iterateObject=(Object[])bean;
+            }else if(bean instanceof Collection) {
+                iterateObject=((Collection)bean).toArray();
+            }else {
+                throw new IllegalArgumentException("Bean is not an array or collection");
+            }
+            Integer rowsValue=ServletParameterHelper.asInteger(rows,null);
+            if(rowsValue==null || rowsValue.intValue()<1) {
+                noOfRows=iterateObject.length;
+            }else {
+                noOfRows=rowsValue.intValue();
+            }
+            Integer colsValue=ServletParameterHelper.asInteger(cols,null);
+            if(colsValue==null || colsValue.intValue()<1) {
+                noOfCols=iterateObject.length;
+            }else {
+                noOfCols=colsValue.intValue();
+            }
+            noOfRowIterations=ServletParameterHelper.asInteger(rowIterations,new Integer(1)).intValue();
+            if(iterateObject.length>0) {
+                index = 0;
+                rowIndex = 0;
+                if(iterateObject[index]!=null) {
+                    pageContext.setAttribute(id,iterateObject[index]);
+                    if(indexId!=null) {
+                        pageContext.setAttribute(indexId,new Integer(index));
+                    }
+                }else {
+                    pageContext.removeAttribute(id);
+                    if(indexId!=null) {
+                        pageContext.removeAttribute(indexId);
+                    }
+                }
+                out.write("<table"+(tableStyle!=null?" class=\""+tableStyle+"\"":"")+(width!=null?" width=\""+width+"\"":"")+(height!=null?" height=\""+height+"\"":"")+">");
+                out.write("<tr>");
+                if(noOfRowIterations<=1) {
+                    out.write("<td"+(cellStyle!=null?" class=\""+cellStyle+"\"":"")+(align!=null?" align=\""+align+"\"":"")+(valign!=null?" valign=\""+valign+"\"":"")+(cellWidth!=null?" width=\""+cellWidth+"\"":"")+(cellHeight!=null?" height=\""+cellHeight+"\"":"")+">");
+                }
+                return EVAL_BODY_INCLUDE;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return SKIP_BODY;
+    }
+
+    public int doAfterBody() throws JspException {
+        try {
+            JspWriter out = pageContext.getOut();
+            if(Log.isEnabled(this,Log.DEBUG)) {
+                Log.println(this,"doAfterBody start: "+StringUtil.objectToString(this,null,TagSupport.class,true));
+            }
+            if(noOfRowIterations<=1) {
+                out.write("</td>");
+            }
+            index++;
+            if(index%noOfCols==0) {
+                out.write("</tr>");
+                rowIndex++;
+                if(rowIndex<noOfRowIterations) {
+                    do {
+                        index--;
+                    }while(index%noOfCols>0);
+                }else {
+                    rowIndex=0;
+                }
+            }else if(index>=iterateObject.length) {
+                out.write("</tr>");
+                rowIndex++;
+                if(rowIndex<noOfRowIterations) {
+                    do {
+                        index--;
+                    }while(index%noOfCols>0);
+                }else {
+                    rowIndex=0;
+                }
+            }
+
+            if(index<iterateObject.length && index<(noOfCols*noOfRows)) {
+                if(index%noOfCols==0) {
+                    out.write("<tr>");
+                }
+                if(noOfRowIterations<=1) {
+                    out.write("<td"+(cellStyle!=null?" class=\""+cellStyle+"\"":"")+(align!=null?" align=\""+align+"\"":"")+(valign!=null?" valign=\""+valign+"\"":"")+(cellWidth!=null?" width=\""+cellWidth+"\"":"")+(cellHeight!=null?" height=\""+cellHeight+"\"":"")+">");
+                }
+                if(iterateObject[index]!=null) {
+                    pageContext.setAttribute(id,iterateObject[index]);
+                    if(indexId!=null) {
+                        pageContext.setAttribute(indexId,new Integer(index));
+                    }
+                }else {
+                    pageContext.removeAttribute(id);
+                    if(indexId!=null) {
+                        pageContext.removeAttribute(indexId);
+                    }
+                }
+                if(Log.isEnabled(this,Log.DEBUG)) {
+                    Log.println(this,"doAfterBody end: "+StringUtil.objectToString(this,null,TagSupport.class,true));
+                }
+                return EVAL_BODY_AGAIN;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return SKIP_BODY;
+    }
+
+    public int doEndTag() throws JspException {
+        try {
+            JspWriter out = pageContext.getOut();
+            if(iterateObject!=null && iterateObject.length>0) {
+                out.write("</table>");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return EVAL_PAGE;
+    }
+
+    public void release() {
+        super.release();
+        rows = null;
+        cols = null;
+        nameRowsCols = null;
+        rowsProperty = null;
+        colsProperty = null;
+        tableStyle = null;
+        rowIterations = null;
+        cellStyle = null;
+        width = null;
+        height = null;
+        cellWidth = null;
+        cellHeight = null;
+        align = null;
+        valign = null;
+        name = null;
+        property = null;
+        id = null;
+        indexId = null;
+
+        iterateObject = null;
+        index = 0;
+        rowIndex = 0;
+        noOfRows = 0;
+        noOfCols = 0;
+        noOfRowIterations = 0;
+    }
+}
