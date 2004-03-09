06b6909727b86ecfcf738b7e86291577b694c891
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2004-03-09 18:56:32 +0000

    Updated categories array from String[] to Integer[] and added support for publishing categories and support for cache date

--- erland-webapp-gallery/trunk/src/erland/webapp/gallery/act/gallery/EditGalleryAction.java
+++ erland-webapp-gallery/trunk/src/erland/webapp/gallery/act/gallery/EditGalleryAction.java
@@ -20,9 +20,11 @@ package erland.webapp.gallery.act.gallery;
  */
 
 import erland.webapp.common.QueryFilter;
+import erland.webapp.common.EntityInterface;
 import erland.webapp.common.act.BaseAction;
 import erland.webapp.gallery.entity.gallery.Gallery;
 import erland.webapp.gallery.entity.gallery.GalleryCategoryAssociation;
+import erland.webapp.gallery.entity.gallery.picture.Picture;
 import erland.webapp.gallery.fb.gallery.GalleryFB;
 import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.struts.action.ActionForm;
@@ -31,6 +33,10 @@ import org.apache.struts.action.ActionForward;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class EditGalleryAction extends BaseAction {
     private final static String ID = EditGalleryAction.class+"-id";
@@ -38,27 +44,72 @@ public class EditGalleryAction extends BaseAction {
     protected void executeLogic(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
         GalleryFB fb = (GalleryFB) form;
         setId(request,fb.getId());
+
         String username = request.getRemoteUser();
         Gallery gallery = (Gallery) getEnvironment().getEntityFactory().create("gallery-gallery");
-        gallery.setUsername(username);
         PropertyUtils.copyProperties(gallery, fb);
+        gallery.setUsername(username);
+
+        Date cacheDate = new Date();
+        if(fb.getId()!=null) {
+            Gallery template = (Gallery) getEnvironment().getEntityFactory().create("gallery-gallery");
+            template.setId(fb.getId());
+            Gallery oldGallery = (Gallery) getEnvironment().getEntityStorageFactory().getStorage("gallery-gallery").load(template);
+            cacheDate = oldGallery.getCacheDate();
+        }
+        gallery.setCacheDate(cacheDate);
+
         getEnvironment().getEntityStorageFactory().getStorage("gallery-gallery").store(gallery);
 
         if (fb.getReferencedGallery() != null && fb.getReferencedGallery().intValue()!=0) {
-            String[] categories = fb.getCategories();
+            Integer[] categories = fb.getCategories();
             if (categories != null) {
                 QueryFilter filter = new QueryFilter("allforgallery");
                 filter.setAttribute("gallery", gallery.getId());
                 getEnvironment().getEntityStorageFactory().getStorage("gallery-gallerycategoryassociation").delete(filter);
                 for (int i = 0; i < categories.length; i++) {
-                    if (categories[i].length() > 0) {
-                        Integer categoryId = Integer.valueOf(categories[i]);
+                    if (categories[i]!=null) {
                         GalleryCategoryAssociation category = (GalleryCategoryAssociation) getEnvironment().getEntityFactory().create("gallery-gallerycategoryassociation");
-                        category.setCategory(categoryId);
+                        category.setCategory(categories[i]);
                         category.setGallery(gallery.getId());
                         getEnvironment().getEntityStorageFactory().getStorage("gallery-gallerycategoryassociation").store(category);
                     }
                 }
+            }
+        }else {
+            QueryFilter filter = new QueryFilter("calculateofficialforgallery");
+            filter.setAttribute("gallery", gallery.getId());
+            updatePictures(filter, gallery.getId(), Boolean.TRUE);
+
+            filter = new QueryFilter("calculateunofficialforgallery");
+            filter.setAttribute("gallery", gallery.getId());
+            updatePictures(filter, gallery.getId(), Boolean.FALSE);
+            if(gallery.getOfficialCategory()!=null && gallery.getOfficialCategory().intValue()!=0) {
+                filter = new QueryFilter("calculateallwithoutcategory");
+                filter.setAttribute("gallery",gallery.getId());
+                filter.setAttribute("category", gallery.getOfficialCategory());
+                updatePictures(filter, gallery.getId(), Boolean.FALSE);
+            }
+            if(gallery.getOfficialGuestCategory()!=null && gallery.getOfficialGuestCategory().intValue()!=0) {
+                filter = new QueryFilter("calculateallwithcategory");
+                filter.setAttribute("gallery",gallery.getId());
+                filter.setAttribute("category", gallery.getOfficialGuestCategory());
+                updatePictures(filter, gallery.getId(), Boolean.FALSE);
+            }
+
+            filter = new QueryFilter("calculateofficialguestforgallery");
+            filter.setAttribute("gallery", gallery.getId());
+            updatePicturesGuest(filter, gallery.getId(), Boolean.TRUE);
+
+            filter = new QueryFilter("calculateunofficialguestforgallery");
+            filter.setAttribute("gallery", gallery.getId());
+            updatePicturesGuest(filter, gallery.getId(), Boolean.FALSE);
+
+            if(gallery.getOfficialGuestCategory()!=null && gallery.getOfficialGuestCategory().intValue()!=0) {
+                filter = new QueryFilter("calculateallunofficialorwithoutcategory");
+                filter.setAttribute("gallery",gallery.getId());
+                filter.setAttribute("category", gallery.getOfficialGuestCategory());
+                updatePicturesGuest(filter, gallery.getId(), Boolean.FALSE);
             }
         }
     }
@@ -76,5 +127,29 @@ public class EditGalleryAction extends BaseAction {
     }
     public void setId(HttpServletRequest request, Integer id) {
         request.setAttribute(ID,id);
+    }
+    private void updatePictures(QueryFilter filter, Integer gallery, Boolean official) {
+        Picture entity = (Picture) getEnvironment().getEntityFactory().create("gallery-picture");
+        entity.setOfficial(official);
+        EntityInterface[] entities = getEnvironment().getEntityStorageFactory().getStorage("gallery-picture").search(filter);
+        updatePictures(entities, gallery, entity);
+    }
+
+    private void updatePicturesGuest(QueryFilter filter, Integer gallery, Boolean official) {
+        Picture entity = (Picture) getEnvironment().getEntityFactory().create("gallery-picture");
+        entity.setOfficialGuest(official);
+        EntityInterface[] entities = getEnvironment().getEntityStorageFactory().getStorage("gallery-picture").search(filter);
+        updatePictures(entities, gallery, entity);
+    }
+
+    private void updatePictures(EntityInterface[] entities, Integer gallery, Picture picture) {
+        Collection pictures = new ArrayList(entities.length);
+        for (int i = 0; i < entities.length; i++) {
+            pictures.add(((Picture) entities[i]).getId());
+        }
+        QueryFilter pictureFilter = new QueryFilter("allforgalleryandpicturelist");
+        pictureFilter.setAttribute("gallery", gallery);
+        pictureFilter.setAttribute("pictures", pictures);
+        getEnvironment().getEntityStorageFactory().getStorage("gallery-picture").update(pictureFilter, picture);
     }
 }
