2e3f1c6b5ce528b6ab725544215ed904c03d81cc
nobody <nobody@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2004-03-21 16:39:39 +0000

    This commit was manufactured by cvs2svn to create tag 'rel_1_7'.

---
+++ erland-webapp-common/tags/rel_1_7/src/erland/webapp/common/act/LogConfigurationPlugin.java
@@ -0,0 +1,61 @@
+package erland.webapp.common.act;
+
+import org.apache.struts.action.PlugIn;
+import org.apache.struts.action.ActionServlet;
+import org.apache.struts.config.ModuleConfig;
+
+import javax.servlet.ServletException;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import erland.util.Log;
+import erland.util.ParameterStorageString;
+import erland.util.StreamStorage;
+
+/*
+ * Copyright (C) 2003 Erland Isaksson (erland_i@hotmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ */
+
+public class LogConfigurationPlugin implements PlugIn {
+    private String logConfigurationFile = "debug_log.xml";
+    public void destroy() {
+        //Do nothing
+    }
+
+    public void init(ActionServlet actionServlet, ModuleConfig moduleConfig) throws ServletException {
+        InputStream input = getClass().getResourceAsStream("/"+logConfigurationFile);
+        if(input==null) {
+            try {
+                input = new FileInputStream(logConfigurationFile);
+            } catch (FileNotFoundException e) {
+                // Do Nothing
+            }
+        }
+        if(input!=null) {
+            System.out.println("Loading log configuration from: "+logConfigurationFile);
+            Log.setLogConfig(new ParameterStorageString(new StreamStorage(input,null),null,"log"));
+        }else {
+            System.out.println("Failed loading log configuration from: "+logConfigurationFile);
+        }
+    }
+
+    public void setLogConfigurationFile(String logConfigurationFile) {
+        this.logConfigurationFile = logConfigurationFile;
+    }
+}
---
+++ erland-webapp-common/tags/rel_1_7/src/erland/webapp/common/image/ImageThumbnail.java
@@ -0,0 +1,85 @@
+package erland.webapp.common.image;
+
+/*
+ * Copyright (C) 2003 Erland Isaksson (erland_i@hotmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageFilter;
+import java.awt.image.ImageProducer;
+import java.awt.image.FilteredImageSource;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class ImageThumbnail implements ThumbnailCreatorInterface {
+    private static Object sync = new Object();
+    private Boolean antialias;
+
+    public ImageThumbnail() {
+        antialias = Boolean.FALSE;
+    }
+    public ImageThumbnail(Boolean antialias) {
+        this.antialias = antialias!=null?antialias:Boolean.FALSE;
+    }
+    public BufferedImage create(URL url, int requestedWidth, ImageFilterContainerInterface filters) throws IOException {
+        BufferedImage thumbnail = null;
+        synchronized (sync) {
+            BufferedImage image = ImageIO.read(new BufferedInputStream(url.openStream()));
+            if (image != null) {
+                ImageFilter[] filterList = filters!=null?filters.getFilters():null;
+                Toolkit toolkit = Toolkit.getDefaultToolkit();
+                if(filterList!=null && filterList.length>0) {
+                    ImageProducer prod = image.getSource();
+                    for (int i = 0; i < filterList.length; i++) {
+                        ImageFilter postFilter = filterList[i];
+                        prod = new FilteredImageSource(prod,postFilter);
+                    }
+                    Image img = toolkit.createImage(prod);
+                    image = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_RGB);
+                    Graphics g = image.createGraphics();
+                    g.drawImage(img, 0, 0, null);
+                    g.dispose();
+                }
+                int width = requestedWidth;
+                int height = width * image.getHeight() / image.getWidth();
+                if (((double) image.getWidth() / image.getHeight()) < ((double) 1600 / 1200)) {
+                    height = width * 1200 / 1600;
+                    width = height * image.getWidth() / image.getHeight();
+                }
+                if (width > image.getWidth() || height > image.getHeight()) {
+                    width = image.getWidth();
+                    height = image.getHeight();
+                }
+                thumbnail = new BufferedImage(width, height, image.getType());
+                Graphics2D g2 = thumbnail.createGraphics();
+                g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+                if(antialias.booleanValue()) {
+                    Image scaled = image.getScaledInstance(width,height,Image.SCALE_AREA_AVERAGING);
+                    g2.drawImage(scaled, 0, 0, null);
+                }else {
+                    g2.drawImage(image, 0, 0, width, height, 0, 0, image.getWidth(), image.getHeight(), null);
+                }
+                g2.dispose();
+            }
+        }
+        return thumbnail;
+    }
+}
