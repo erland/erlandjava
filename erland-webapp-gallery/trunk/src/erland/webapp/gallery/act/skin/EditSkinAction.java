10e021fe15f479b5d2bc8915b117e39366a45a0d
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2004-03-29 21:33:33 +0000

    Added error handling which checks if files exist

--- erland-webapp-gallery/trunk/src/erland/webapp/gallery/act/skin/EditSkinAction.java
+++ erland-webapp-gallery/trunk/src/erland/webapp/gallery/act/skin/EditSkinAction.java
@@ -24,18 +24,41 @@ import erland.webapp.gallery.entity.gallery.picturestorage.PictureStorage;
 import erland.webapp.gallery.entity.skin.Skin;
 import erland.webapp.gallery.fb.gallery.picturestorage.PictureStorageFB;
 import erland.webapp.gallery.fb.skin.SkinFB;
+import erland.util.StringUtil;
 import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.util.Arrays;
 
 public class EditSkinAction extends BaseAction {
     protected void executeLogic(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
         SkinFB fb = (SkinFB) form;
-        Skin storage = (Skin) getEnvironment().getEntityFactory().create("gallery-skin");
-        PropertyUtils.copyProperties(storage, fb);
-        getEnvironment().getEntityStorageFactory().getStorage("gallery-skin").store(storage);
+        if(!fileExists(fb.getLayout()) ||
+           !fileExists(fb.getLayoutSingle()) ||
+           !fileExists(fb.getMenu()) ||
+           !fileExists(fb.getHeader()) ||
+           !fileExists(fb.getSearch()) ||
+           !fileExists(fb.getViewPicture()) ||
+           !fileExists(fb.getViewPictures())) {
+
+            saveErrors(request, Arrays.asList(new String[]{"gallery.skin.edit.file-dont-exist"}));
+            return;
+        }
+        Skin skin = (Skin) getEnvironment().getEntityFactory().create("gallery-skin");
+        PropertyUtils.copyProperties(skin, fb);
+        getEnvironment().getEntityStorageFactory().getStorage("gallery-skin").store(skin);
+    }
+    private boolean fileExists(String filename) {
+        if(StringUtil.asNull(filename)!=null) {
+            File file = new File(getServlet().getServletContext().getRealPath(filename));
+            if(!file.exists()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
