9286eaa106377e3f91d08f203e5214e737885125
nobody <nobody@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2004-03-29 21:44:56 +0000

    This commit was manufactured by cvs2svn to create tag 'rel_1_9'.

---
+++ erland-webapp-common/tags/rel_1_9/src/erland/webapp/common/fb/BaseFB.java
@@ -0,0 +1,40 @@
+package erland.webapp.common.fb;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.validator.ValidatorForm;
+import org.apache.commons.beanutils.PropertyUtils;
+
+import java.util.Map;
+import java.util.Iterator;
+import java.util.Collection;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+import erland.util.StringUtil;
+
+/*
+ * Copyright (C) 2004 Erland Isaksson (erland_i@hotmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ */
+
+public class BaseFB extends ValidatorForm {
+    public String toString() {
+        return StringUtil.beanToString(this,".*Display",ValidatorForm.class,true);
+    }
+}
