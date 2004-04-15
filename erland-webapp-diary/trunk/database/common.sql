fb67ccdc4d0dbb1f74283b69e07daaea140f6646
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2004-04-15 20:59:15 +0000

    Updated according to 1.10 of erland-webapp-common library database structure

--- erland-webapp-diary/trunk/database/common.sql
+++ erland-webapp-diary/trunk/database/common.sql
@@ -1,22 +1,23 @@
--- MySQL dump 9.09
+-- MySQL dump 9.08
 --
 -- Host: localhost    Database: common
 ---------------------------------------------------------
--- Server version	4.0.15-max-debug
+-- Server version	4.0.13-max-nt
 
 --
--- Table structure for table `descriptiontags`
+-- Table structure for table 'descriptiontags'
 --
 
 CREATE TABLE descriptiontags (
   type varchar(100) NOT NULL default '',
   tag varchar(100) NOT NULL default '',
-  description longtext NOT NULL,
+  description longtext NOT NULL default '',
+  description_en longtext NOT NULL default '',
   PRIMARY KEY  (type,tag)
 ) TYPE=MyISAM;
 
 --
--- Table structure for table `resources`
+-- Table structure for table 'resources'
 --
 
 CREATE TABLE resources (
--- erland-webapp-diary/trunk/database/common_data.sql
+++ erland-webapp-diary/trunk/database/common_data.sql
@@ -1,13 +1,14 @@
--- MySQL dump 9.09
+-- MySQL dump 9.08
 --
 -- Host: localhost    Database: common
 ---------------------------------------------------------
--- Server version	4.0.15-max-debug
+-- Server version	4.0.13-max-nt
 
 --
--- Dumping data for table `resources`
+-- Dumping data for table 'resources'
 --
 
 INSERT INTO resources VALUES ('diary','resources.thumbnail.cache','D%3A%5Cusers%5Cerland%5Cthumbnails');
 INSERT INTO resources VALUES ('diary','resources.welcometext','Logga in för att uppdatera din dagbok eller klicka [boldlink=här,http://erland.homeip.net/diary/do/guest/home?user=erland\"] för att läsa min dagbok. Du kan också välja att titta på någon annans dagbok genom att klicka på länkarna nedan.\r\nEller registrera din egen dagbok genom att klicka [boldlink=här,/diary/do/guest/registeruser]\r\n\r\n');
+INSERT INTO resources VALUES ('diary','resources.nativelanguage','sv');
 
