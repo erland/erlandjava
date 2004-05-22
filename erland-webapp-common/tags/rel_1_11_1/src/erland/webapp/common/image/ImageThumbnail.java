30982f6b5839380750c007d6e106735cb605ae1c
nobody <nobody@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2004-05-22 12:29:38 +0000

    This commit was manufactured by cvs2svn to create tag 'rel_1_11_1'.

---
+++ erland-webapp-common/tags/rel_1_11_1/src/erland/webapp/common/image/ImageThumbnail.java
@@ -0,0 +1,123 @@
+package erland.webapp.common.image;
+
+/*
+ * Copyright (C) 2003 Erland Isaksson (erland_i@hotmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+import erland.util.Log;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.color.ICC_ColorSpace;
+import java.awt.geom.AffineTransform;
+import java.awt.image.*;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.net.URL;
+
+public class ImageThumbnail implements ThumbnailCreatorInterface {
+    private static Object sync = new Object();
+    private Boolean antialias;
+
+    public ImageThumbnail() {
+        antialias = Boolean.FALSE;
+    }
+    public ImageThumbnail(Boolean antialias) {
+        this.antialias = antialias!=null?antialias:Boolean.FALSE;
+    }
+    public BufferedImage create(URL url, int requestedWidth, int requestedHeight, ImageFilterContainerInterface filters) throws IOException {
+        BufferedImage thumbnail = null;
+        synchronized (sync) {
+            Log.println(this,"Opening "+url.getFile());
+            BufferedImage image = ImageIO.read(new BufferedInputStream(url.openStream()));
+            if(Log.isEnabled(this,Log.DEBUG)) {
+                Log.println(this,"Opened image "+image,Log.DEBUG);
+            }
+            if (image != null) {
+                ImageFilter[] filterList = filters!=null?filters.getFilters():null;
+                Toolkit toolkit = Toolkit.getDefaultToolkit();
+                if(filterList!=null && filterList.length>0) {
+                    Log.println(this,"Applying filters",Log.DEBUG);
+                    ImageProducer prod = image.getSource();
+                    for (int i = 0; i < filterList.length; i++) {
+                        ImageFilter postFilter = filterList[i];
+                        prod = new FilteredImageSource(prod,postFilter);
+                    }
+                    Image img = toolkit.createImage(prod);
+                    image = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_RGB);
+                    Log.println(this,"Filters applied",Log.DEBUG);
+                    Graphics g = image.createGraphics();
+                    Log.println(this,"Draw image",Log.DEBUG);
+                    g.drawImage(img, 0, 0, null);
+                    g.dispose();
+                }
+                int width = requestedWidth;
+                int height = requestedHeight;
+                int origWidth = image.getWidth();
+                int origHeight = image.getHeight();
+                if(height==0) {
+                    height = width * origHeight / origWidth;
+                    if (((double) origWidth / origHeight) < ((double) 1600 / 1200)) {
+                        height = width * 1200 / 1600;
+                        width = height * origWidth / origHeight;
+                    }
+                }else {
+                    if (((double) origWidth / origHeight < ((double) width/height))) {
+                        width = height * origWidth / origHeight;
+                    }else {
+                        height = width * origHeight / origWidth;
+                    }
+                }
+                if (width > origWidth || height > origHeight) {
+                    width = origWidth;
+                    height = origHeight;
+                }
+                Log.println(this,"Creating BufferedImage for thumbnail",Log.DEBUG);
+                // The next three rows are quite stupid, but it was the only way to get decent performance
+                // on images editied in PhotoShop and saved with a ICC profile.
+                BufferedImage tempImage = new BufferedImage(origWidth, origHeight, image.getType());
+                //tempImage.setData(image.getRaster());
+                tempImage.getGraphics().drawImage(image,0,0,null);
+                image = tempImage;
+
+                thumbnail = new BufferedImage(width, height, image.getType());
+                Log.println(this,"Create graphics",Log.DEBUG);
+                Graphics2D g2 = thumbnail.createGraphics();
+                g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+                if(antialias.booleanValue()) {
+                    Log.println(this,"Creating scaled instance",Log.DEBUG);
+                    Image scaled = image.getScaledInstance(width,height,Image.SCALE_AREA_AVERAGING);
+                    Log.println(this,"Drawing thumbnail",Log.DEBUG);
+                    g2.drawImage(scaled, 0, 0, null);
+                    Log.println(this,"Thumbnail drawed",Log.DEBUG);
+                }else {
+                    Log.println(this,"Drawing thumbnail",Log.DEBUG);
+                    g2.drawImage(image, 0, 0, width, height, 0, 0, origWidth, origHeight, null);
+                    Log.println(this,"Thumbnail drawed",Log.DEBUG);
+                }
+                g2.dispose();
+                Log.println(this,"Thumbnail successfully generated",Log.DEBUG);
+            }
+        }
+        if(Log.isEnabled(this,Log.DEBUG)) {
+            Log.println(this,"Returning thumbnail "+thumbnail,Log.DEBUG);
+        }
+        return thumbnail;
+    }
+}
---
+++ erland-webapp-common/tags/rel_1_11_1/src/erland/webapp/common/image/ImageWriteHelper.java
@@ -0,0 +1,239 @@
+package erland.webapp.common.image;
+
+/*
+ * Copyright (C) 2003 Erland Isaksson (erland_i@hotmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+import erland.webapp.common.WebAppEnvironmentInterface;
+import erland.util.Log;
+
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.*;
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Iterator;
+import java.util.Date;
+
+public class ImageWriteHelper {
+    private static final int THUMBNAIL_WIDTH = 150;
+    private static final int COPYRIGHT_WIDTH = 640;
+    private static final int COPYRIGHT_HEIGHT = 480;
+    private static final float COMPRESSION = 0.9f;
+    private static final float SMALL_THUMBNAIL_COMPRESSION = 0.5f;
+    private static final int COPYRIGHT_OFFSET = 5;
+    private static ImageWriteHelper me;
+
+    private ImageWriteHelper() {};
+
+    public static boolean writeImage(WebAppEnvironmentInterface environment,String file, OutputStream output) {
+        try {
+            if (file != null) {
+                BufferedInputStream input = null;
+                Log.println(getLogInstance(), "Loading image: " + file);
+                 try {
+                    input = new BufferedInputStream(new FileInputStream(file));
+                } catch (FileNotFoundException e) {
+                    input = new BufferedInputStream(new URL(file).openConnection().getInputStream());
+                }
+                write(input, output);
+                input.close();
+                return true;
+            } else {
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+    public static boolean writeThumbnail(WebAppEnvironmentInterface environment,Integer width, Boolean useCache, Float compression, String username, String imageFile, String copyrightText, ThumbnailCreatorInterface thumbnailCreator, OutputStream output) {
+        return writeThumbnail(environment,width,null,useCache,compression,username,imageFile,copyrightText,thumbnailCreator,null,null,null,null,output);
+    }
+
+    public static boolean writeThumbnail(WebAppEnvironmentInterface environment,Integer width, Integer height, Boolean useCache, Float compression, String username, String imageFile, String copyrightText, ThumbnailCreatorInterface thumbnailCreator, OutputStream output) {
+        return writeThumbnail(environment,width,height,useCache,compression,username,imageFile,copyrightText,thumbnailCreator,null,null,null,null,output);
+    }
+    public static boolean writeThumbnail(WebAppEnvironmentInterface environment,Integer width, Boolean useCache, Float compression, String username, String imageFile, String copyrightText, ThumbnailCreatorInterface thumbnailCreator, String cachePrefix, ImageFilterContainerInterface preFilters, ImageFilterContainerInterface postFilters, Date cacheDate, OutputStream output) {
+        return writeThumbnail(environment,width,null,useCache,compression,username,imageFile,copyrightText,thumbnailCreator,null,null,null,null,output);
+    }
+    public static boolean writeThumbnail(WebAppEnvironmentInterface environment,Integer width, Integer height, Boolean useCache, Float compression, String username, String imageFile, String copyrightText, ThumbnailCreatorInterface thumbnailCreator, String cachePrefix, ImageFilterContainerInterface preFilters, ImageFilterContainerInterface postFilters, Date cacheDate, OutputStream output) {
+        Log.println(getLogInstance(), "Loading thumbnail image: " + imageFile);
+        String cacheDir = environment.getConfigurableResources().getParameter("thumbnail.cache");
+        int requestedWidth = width!=null?width.intValue():THUMBNAIL_WIDTH;
+        int requestedHeight = height!=null?height.intValue():0;
+        float requestedCompression = compression!=null?compression.floatValue():0f;
+        if (requestedCompression == 0) {
+            requestedCompression = requestedWidth <= THUMBNAIL_WIDTH ? SMALL_THUMBNAIL_COMPRESSION : COMPRESSION;
+        }
+        if (requestedCompression > 1.0f) {
+            requestedCompression = 1.0f;
+        } else if (requestedCompression < 0.0f) {
+            requestedCompression = 0.0f;
+        }
+        BufferedImage thumbnail = null;
+        try {
+            if (imageFile != null) {
+                URL url = null;
+                long lastModified = 0;
+                File file = new File(imageFile);
+                if (file.exists()) {
+                    lastModified = file.lastModified();
+                    url = file.toURL();
+                } else {
+                    url = new URL(imageFile);
+                    URLConnection connection = url.openConnection();
+                    lastModified = connection.getLastModified();
+                }
+
+                File cachedFile = null;
+                if (useCache.booleanValue()) {
+                    cachedFile = getFromCache(cacheDir, username, cachePrefix, requestedWidth, imageFile, lastModified);
+                }
+                if (cachedFile != null && (cacheDate==null||cacheDate.getTime()<cachedFile.lastModified())) {
+                    InputStream inputCache = new FileInputStream(cachedFile);
+                    write(inputCache, output);
+                    inputCache.close();
+                    return true;
+                } else {
+                    thumbnail = thumbnailCreator.create(url, requestedWidth, requestedHeight, preFilters);
+                    ImageFilter[] filters = postFilters!=null?postFilters.getFilters():null;
+                    if(filters!=null && filters.length>0) {
+                        Log.println(getLogInstance(),"Applying filters",Log.DEBUG);
+                        ImageProducer prod = thumbnail.getSource();
+                        for (int i = 0; i < filters.length; i++) {
+                            ImageFilter postFilter = filters[i];
+                            prod = new FilteredImageSource(prod,postFilter);
+                        }
+                        Image img = Toolkit.getDefaultToolkit().createImage(prod);
+                        thumbnail = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_RGB);
+                        Graphics g = thumbnail.createGraphics();
+                        g.drawImage(img, 0, 0, null);
+                        g.dispose();
+                        Log.println(getLogInstance(),"Filters applied",Log.DEBUG);
+                    }
+                    Graphics2D g2= thumbnail.createGraphics();
+                    int finalWidth = thumbnail.getWidth();
+                    int finalHeight = thumbnail.getHeight();
+                    if (finalWidth >= COPYRIGHT_WIDTH || finalHeight>= COPYRIGHT_HEIGHT) {
+                        if (copyrightText != null && copyrightText.length() > 0) {
+                            Log.println(getLogInstance(),"Drawing copyright",Log.DEBUG);
+                            FontMetrics metrics = g2.getFontMetrics();
+                            Rectangle2D rc = metrics.getStringBounds(copyrightText, g2);
+                            g2.setColor(new Color(1f, 1f, 1f, 0.4f));
+                            g2.fill3DRect(finalWidth - (int) rc.getWidth() - COPYRIGHT_OFFSET * 3, finalHeight - (int) rc.getHeight() - COPYRIGHT_OFFSET * 3, (int) rc.getWidth() + COPYRIGHT_OFFSET * 2, (int) rc.getHeight() + COPYRIGHT_OFFSET * 2, true);
+                            g2.setColor(Color.black);
+                            g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+                            g2.drawString(copyrightText, finalWidth - (int) rc.getWidth() - COPYRIGHT_OFFSET * 2, finalHeight - COPYRIGHT_OFFSET * 2 - metrics.getDescent());
+                            Log.println(getLogInstance(),"Copyright drawed",Log.DEBUG);
+                        }
+                    }
+
+                    if (thumbnail != null) {
+                        Log.println(getLogInstance(),"Create thumbnail jpeg",Log.DEBUG);
+                        ImageOutputStream imageOutput = ImageIO.createImageOutputStream(output);
+                        Log.println(getLogInstance(),"Write thumbnail to response",Log.DEBUG);
+                        writeImageToOutput(thumbnail, requestedCompression, imageOutput);
+                        Log.println(getLogInstance(),"Thumbnail written to response",Log.DEBUG);
+                        imageOutput.close();
+
+                        if (useCache.booleanValue()) {
+                            setInCache(cacheDir,username, cachePrefix, requestedWidth, imageFile, thumbnail, requestedCompression);
+                        }
+                        return true;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+    private static void write(InputStream input, OutputStream output) throws IOException {
+        byte[] data = new byte[100000];
+        while (true) {
+            int length = input.read(data);
+            if (length < 0) {
+                return;
+            }
+            output.write(data, 0, length);
+        }
+    }
+    public static void writeImageToOutput(BufferedImage image, OutputStream output) throws IOException {
+        float compression = image.getWidth() <= THUMBNAIL_WIDTH ? SMALL_THUMBNAIL_COMPRESSION : COMPRESSION;
+        writeImageToOutput(image,compression,output);
+    }
+
+    public static void writeImageToOutput(BufferedImage image, float compression, OutputStream output) throws IOException {
+        ImageOutputStream imageOutput = ImageIO.createImageOutputStream(output);
+        writeImageToOutput(image,compression,imageOutput);
+    }
+    private static void writeImageToOutput(BufferedImage image, float requestedCompression, ImageOutputStream output) throws IOException {
+        Iterator it = ImageIO.getImageWritersByFormatName("JPEG");
+        ImageWriter writer = (ImageWriter) it.next();
+        ImageWriteParam param = writer.getDefaultWriteParam();
+        param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+        param.setCompressionQuality(requestedCompression);
+        writer.setOutput(output);
+        writer.write(null, new IIOImage(image, null, null), param);
+        writer.dispose();
+    }
+
+    private static String getCacheFileName(String username, String cachePrefix, int width, String file) {
+        return username + "_" + (cachePrefix!=null?cachePrefix + "_":"") + width + "_" + file.replaceAll("[\\\\:/]", "_");
+    }
+
+    private static File getFromCache(String cacheDir,String username, String cachePrefix, int width, String originalFile, long lastModified) {
+        String cacheFileName = getCacheFileName(username, cachePrefix, width, originalFile);
+        Log.println(getLogInstance(), "Loading thumbnail from cache: " + cacheDir+"/"+cacheFileName);
+        File cachedfile = new File(cacheDir + "/" + cacheFileName);
+        if (cachedfile.exists()) {
+            if (cachedfile.lastModified() > lastModified) {
+                return cachedfile;
+            }
+        }
+        return null;
+    }
+
+    private static void setInCache(String cacheDir,String username, String cachePrefix, int width, String originalFile, BufferedImage image, float compression) {
+        try {
+            String cacheFileName = getCacheFileName(username, cachePrefix, width, originalFile);
+            ImageOutputStream output = ImageIO.createImageOutputStream(new File(cacheDir + "/" + cacheFileName));
+            writeImageToOutput(image, compression, output);
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static ImageWriteHelper getLogInstance() {
+        if(me==null) {
+            me = new ImageWriteHelper();
+        }
+        return me;
+    }
+}
