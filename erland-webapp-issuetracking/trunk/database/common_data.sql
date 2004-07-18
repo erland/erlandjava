d150c0fe88e73f91b7d8ddd7bc6782e46acb24e4
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2004-07-18 07:24:01 +0000

    Added english welcometext and updated welcometext links so they opens in same window.

--- erland-webapp-issuetracking/trunk/database/common_data.sql
+++ erland-webapp-issuetracking/trunk/database/common_data.sql
@@ -8,7 +8,8 @@
 -- Dumping data for table 'resources'
 --
 
-INSERT INTO resources VALUES ('issuetracking','resources.welcometext','Logga in för att hantera dina felrapporter och förbättringsförslag. Du kan även registrera din egen användare genom att klicka [boldlink=här,/issuetracking/do/guest/registeruser].\r\n\r\nVill du rapportera en ny problemrapport skall du klicka [boldlink=här,/issuetracking/do/guest/newissue]\r\n\r\n');
+INSERT INTO resources VALUES ('issuetracking','resources.welcometext','Login to manager your problem reports and feature requests. You can also register your own user by clicking [boldlinksamewindow=here,/issuetracking/do/guest/registeruser].\r\n\r\nIf you want to report a new problem report or feature request you should click [boldlinksamewindow=here,/issuetracking/do/guest/newissue]\r\n\r\n');
+INSERT INTO resources VALUES ('issuetracking','resources.welcometext_sv','Logga in för att hantera dina felrapporter och förbättringsförslag. Du kan även registrera din egen användare genom att klicka [boldlinksamewindow=här,/issuetracking/do/guest/registeruser].\r\n\r\nVill du rapportera en ny problemrapport skall du klicka [boldlinksamewindow=här,/issuetracking/do/guest/newissue]\r\n\r\n');
 INSERT INTO resources VALUES ('issuetracking','resources.nativelanguage','sv');
 
 
