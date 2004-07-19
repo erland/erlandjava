a824391f9d5b343cbae5e46c605b160205b03342
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2004-07-19 15:09:41 +0000

    Changed name of sb to sb_aktie and added sb_obligation cache dir

--- erland-webapp-stock/trunk/database/common_data.sql
+++ erland-webapp-stock/trunk/database/common_data.sql
@@ -9,7 +9,8 @@
 --
 
 INSERT INTO resources VALUES ('stock','resources.brokers.robur.cache','D%3A%5Cusers%5Cerland%5Crobur');
-INSERT INTO resources VALUES ('stock','resources.brokers.sb.cache','D%3A%5Cusers%5Cerland%5Crobur');
+INSERT INTO resources VALUES ('stock','resources.brokers.sb_aktie.cache','D%3A%5Cusers%5Cerland%5Crobur');
+INSERT INTO resources VALUES ('stock','resources.brokers.sb_obligation.cache','D%3A%5Cusers%5Cerland%5Crobur');
 INSERT INTO resources VALUES ('stock','resources.welcometext','Login to look at your stock diagrams or to update your stock account\r\n\r\nIf you want to have access to the stock diagram page please contact [boldlink=me,mailto:erland.i@telia.com]\r\n\r\n');
 INSERT INTO resources VALUES ('stock','resources.welcometext_sv','Logga in för att titta på dina aktiediagram eller för att uppdatera dina aktieinnehav\r\n\r\nOm du vill ha tillgång till aktiediagramsidan kan du höra av dig till [boldlink=mig,mailto:erland.i@telia.com]\r\n\r\n');
 
