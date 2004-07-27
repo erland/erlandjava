2a91521d804338d0821dcc32ecddb8ddb9e65d66
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2004-07-27 13:55:20 +0000

    Updated drawing logic

--- erland-game-isoadventure/trunk/src/erland/game/isoadventure/IsoMap.java
+++ erland-game-isoadventure/trunk/src/erland/game/isoadventure/IsoMap.java
@@ -39,39 +39,79 @@ public class IsoMap implements MapDrawInterface {
     public void draw(Graphics g) {
         if(maps.size()>0) {
             IsoObjectMapInterface main = (IsoObjectMapInterface) maps.elementAt(0);
-            for(int z=0;z<main.getSizeZ();z++) {
-                int noOfRows = main.getSizeX();
-                for(int row=0;row<noOfRows;row++) {
-                    for(int x=0,y=row;y>=0;y--,x++) {
+            int noOfRows = main.getSizeX();
+            int prevX = 0;
+            int prevY = 0;
+            for(int row=0;row<noOfRows;row++) {
+                for(int x=0,y=row;y>=0;y--,x++) {
+                    boolean bRedraw = false;
+                    for(int z=0;z<main.getSizeZ();z++) {
+                        drawTile(main, x, y, z, g);
                         IsoObjectInterface obj = main.getObject(x,y,z);
-                        if(obj!=null) {
-                            obj.draw(g);
-                        }
-                        for(int i=1;i<maps.size();i++) {
-                            IsoObjectMapInterface map = (IsoObjectMapInterface) maps.elementAt(i);
-                            obj = map.getObject(x,y,z);
-                            if(obj!=null) {
-                                obj.draw(g);
+                        if(!bRedraw && obj!=null && (obj.getPosX()>obj.getMovingPosX())) {
+                            bRedraw = true;
+                        }else if(!bRedraw) {
+                            for(int i=1;i<maps.size();i++) {
+                                IsoObjectMapInterface map = (IsoObjectMapInterface) maps.elementAt(i);
+                                obj = map.getObject(x,y,z);
+                                if(!bRedraw && obj!=null && (obj.getPosX()<obj.getMovingPosX())) {
+                                    bRedraw = true;
+                                    break;
+                                }
                             }
                         }
                     }
-                }
-                int noOfColumns = main.getSizeY()-1;
-                for(int column=1;column<=noOfColumns;column++) {
-                    for(int x=column,y=noOfColumns;y>=column;y--,x++) {
-                        IsoObjectInterface obj = main.getObject(x,y,z);
-                        if(obj!=null) {
-                            obj.draw(g);
+                    if(bRedraw) {
+                        for(int z=0;z<main.getSizeZ();z++) {
+                            drawTile(main, prevX, prevY, z, g);
                         }
-                        for(int i=1;i<maps.size();i++) {
-                            IsoObjectMapInterface map = (IsoObjectMapInterface) maps.elementAt(i);
-                            obj = map.getObject(x,y,z);
-                            if(obj!=null) {
-                                obj.draw(g);
+                    }
+                    prevX = x;
+                    prevY = y;
+                }
+            }
+            int noOfColumns = main.getSizeY()-1;
+            for(int column=1;column<=noOfColumns;column++) {
+                for(int x=column,y=noOfColumns;y>=column;y--,x++) {
+                    boolean bRedraw = false;
+                    for(int z=0;z<main.getSizeZ();z++) {
+                        drawTile(main, x, y, z, g);
+                        IsoObjectInterface obj = main.getObject(x,y,z);
+                        if(!bRedraw && obj!=null && (obj.getPosX()>obj.getMovingPosX())) {
+                            bRedraw = true;
+                        }else if(!bRedraw) {
+                            for(int i=1;i<maps.size();i++) {
+                                IsoObjectMapInterface map = (IsoObjectMapInterface) maps.elementAt(i);
+                                obj = map.getObject(x,y,z);
+                                if(!bRedraw && obj!=null && (obj.getPosX()<obj.getMovingPosX())) {
+                                    bRedraw = true;
+                                    break;
+                                }
                             }
                         }
                     }
+                    if(bRedraw) {
+                        for(int z=0;z<main.getSizeZ();z++) {
+                            drawTile(main, prevX, prevY, z, g);
+                        }
+                    }
+                    prevX = x;
+                    prevY = y;
                 }
+            }
+        }
+    }
+
+    private void drawTile(IsoObjectMapInterface main, int x, int y, int z, Graphics g) {
+        IsoObjectInterface obj = main.getObject(x,y,z);
+        if(obj!=null) {
+            obj.draw(g);
+        }
+        for(int i=1;i<maps.size();i++) {
+            IsoObjectMapInterface map = (IsoObjectMapInterface) maps.elementAt(i);
+            obj = map.getObject(x,y,z);
+            if(obj!=null) {
+                obj.draw(g);
             }
         }
     }
