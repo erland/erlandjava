281a85fb4d8a60aa46b8d8416659e05a10648c41
nobody <nobody@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2004-07-27 19:35:33 +0000

    This commit was manufactured by cvs2svn to create tag 'rel_1_1_4'.

---
+++ erland-webapp-download/tags/rel_1_1_4/src/erland/webapp/download/fb/ApplicationVersionPB.java
@@ -0,0 +1,91 @@
+package erland.webapp.download.fb;
+
+import java.util.Date;
+
+import erland.webapp.common.fb.BasePB;
+import erland.webapp.common.ServletParameterHelper;
+import erland.util.StringUtil;
+
+/*
+ * Copyright (C) 2003 Erland Isaksson (erland_i@hotmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ */
+
+public class ApplicationVersionPB extends BasePB {
+    private Date date;
+    private ApplicationFileFB[] files;
+    private String name;
+    private String title;
+    private String version;
+    private String description;
+
+    public ApplicationFileFB[] getFiles() {
+        return files;
+    }
+
+    public void setFiles(ApplicationFileFB[] files) {
+        this.files = files;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDateDisplay() {
+        return StringUtil.asString(date,null);
+    }
+
+    public void setDateDisplay(String dateDisplay) {
+        this.date = StringUtil.asDate(dateDisplay,null);
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
