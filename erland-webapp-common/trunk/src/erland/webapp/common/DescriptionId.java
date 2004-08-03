096299821a6f165954ade3449324361f5e334371
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2004-08-03 15:24:37 +0000

    Moved from diary webapp

---
+++ erland-webapp-common/trunk/src/erland/webapp/common/DescriptionId.java
@@ -0,0 +1,62 @@
+package erland.webapp.common;
+/*
+ * Copyright (C) 2003-2004 Erland Isaksson (erland_i@hotmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+import erland.webapp.common.EntityInterface;
+import erland.webapp.common.WebAppEnvironmentInterface;
+import erland.webapp.common.BaseEntity;
+
+public class DescriptionId extends BaseEntity {
+    private String type;
+    private Integer id;
+    private String description;
+    private String descriptionEnglish;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescriptionEnglish() {
+        return descriptionEnglish;
+    }
+
+    public void setDescriptionEnglish(String descriptionEnglish) {
+        this.descriptionEnglish = descriptionEnglish;
+    }
+}
