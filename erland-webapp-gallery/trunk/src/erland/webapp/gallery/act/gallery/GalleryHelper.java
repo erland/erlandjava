041229c41ce2fce6e51599873ecb28e96368d38d
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2004-08-22 10:05:07 +0000

    Added updatePictureVisibility, moved from EditGalleryAction

--- erland-webapp-gallery/trunk/src/erland/webapp/gallery/act/gallery/GalleryHelper.java
+++ erland-webapp-gallery/trunk/src/erland/webapp/gallery/act/gallery/GalleryHelper.java
@@ -23,9 +23,12 @@ import erland.webapp.common.WebAppEnvironmentInterface;
 import erland.webapp.common.QueryFilter;
 import erland.webapp.common.EntityInterface;
 import erland.webapp.gallery.entity.gallery.Gallery;
+import erland.webapp.gallery.entity.gallery.picture.Picture;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.ArrayList;
 
 public class GalleryHelper {
     public static Integer getGalleryId(WebAppEnvironmentInterface environment, HttpServletRequest request) {
@@ -83,5 +86,71 @@ public class GalleryHelper {
         filter.setAttribute("username", username);
         EntityInterface[] entities = environment.getEntityStorageFactory().getStorage(entityName).search(filter);
         return (Gallery[]) Arrays.asList(entities).toArray(new Gallery[0]);
+    }
+
+    public static void updatePictureVisibility(WebAppEnvironmentInterface environment, Integer galleryId) {
+        updatePictureVisibility(environment,getGallery(environment,galleryId));
+    }
+    public static void updatePictureVisibility(WebAppEnvironmentInterface environment, Gallery gallery) {
+        QueryFilter filter = new QueryFilter("calculateofficialforgallery");
+        filter.setAttribute("gallery", gallery.getId());
+        updatePictures(environment,filter, gallery.getId(), Boolean.TRUE);
+
+        filter = new QueryFilter("calculateunofficialforgallery");
+        filter.setAttribute("gallery", gallery.getId());
+        updatePictures(environment,filter, gallery.getId(), Boolean.FALSE);
+        if(gallery.getOfficialCategory()!=null && gallery.getOfficialCategory().intValue()!=0) {
+            filter = new QueryFilter("calculateallwithoutcategory");
+            filter.setAttribute("gallery",gallery.getId());
+            filter.setAttribute("category", gallery.getOfficialCategory());
+            updatePictures(environment,filter, gallery.getId(), Boolean.FALSE);
+        }
+        if(gallery.getOfficialGuestCategory()!=null && gallery.getOfficialGuestCategory().intValue()!=0) {
+            filter = new QueryFilter("calculateallwithcategory");
+            filter.setAttribute("gallery",gallery.getId());
+            filter.setAttribute("category", gallery.getOfficialGuestCategory());
+            updatePictures(environment,filter, gallery.getId(), Boolean.FALSE);
+        }
+
+        filter = new QueryFilter("calculateofficialguestforgallery");
+        filter.setAttribute("gallery", gallery.getId());
+        updatePicturesGuest(environment,filter, gallery.getId(), Boolean.TRUE);
+
+        filter = new QueryFilter("calculateunofficialguestforgallery");
+        filter.setAttribute("gallery", gallery.getId());
+        updatePicturesGuest(environment,filter, gallery.getId(), Boolean.FALSE);
+
+        if(gallery.getOfficialGuestCategory()!=null && gallery.getOfficialGuestCategory().intValue()!=0) {
+            filter = new QueryFilter("calculateallunofficialorwithoutcategory");
+            filter.setAttribute("gallery",gallery.getId());
+            filter.setAttribute("category", gallery.getOfficialGuestCategory());
+            updatePicturesGuest(environment,filter, gallery.getId(), Boolean.FALSE);
+        }
+    }
+    private static void updatePictures(WebAppEnvironmentInterface environment, QueryFilter filter, Integer gallery, Boolean official) {
+        Picture entity = (Picture) environment.getEntityFactory().create("gallery-picture");
+        entity.setOfficial(official);
+        EntityInterface[] entities = environment.getEntityStorageFactory().getStorage("gallery-picture").search(filter);
+        updatePictures(environment,entities, gallery, entity);
+    }
+
+    private static void updatePicturesGuest(WebAppEnvironmentInterface environment, QueryFilter filter, Integer gallery, Boolean official) {
+        Picture entity = (Picture) environment.getEntityFactory().create("gallery-picture");
+        entity.setOfficialGuest(official);
+        EntityInterface[] entities = environment.getEntityStorageFactory().getStorage("gallery-picture").search(filter);
+        updatePictures(environment,entities, gallery, entity);
+    }
+
+    private static void updatePictures(WebAppEnvironmentInterface environment, EntityInterface[] entities, Integer gallery, Picture picture) {
+        Collection pictures = new ArrayList(entities.length);
+        for (int i = 0; i < entities.length; i++) {
+            pictures.add(((Picture) entities[i]).getId());
+        }
+        if (pictures.size() > 0) {
+	        QueryFilter pictureFilter = new QueryFilter("allforgalleryandpicturelist");
+	        pictureFilter.setAttribute("gallery", gallery);
+	        pictureFilter.setAttribute("pictures", pictures);
+	        environment.getEntityStorageFactory().getStorage("gallery-picture").update(pictureFilter, picture);
+	    }
     }
 }
