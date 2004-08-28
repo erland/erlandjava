678bc8a520241e82abdbf3d8e90d270835b69f99
nobody <nobody@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2004-08-28 10:28:12 +0000

    This commit was manufactured by cvs2svn to create tag 'rel_1_3'.

---
+++ erland-webapp-homepage/tags/rel_1_3/src/erland/webapp/homepage/entity/account/UserAccount.java
@@ -0,0 +1,187 @@
+package erland.webapp.homepage.entity.account;
+
+/*
+ * Copyright (C) 2003-2004 Erland Isaksson (erland_i@hotmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+import erland.webapp.common.BaseEntity;
+
+public class UserAccount extends BaseEntity {
+    private String username;
+    private String title;
+    private String titleEnglish;
+    private String headerText;
+    private String headerTextEnglish;
+    private String description;
+    private String descriptionEnglish;
+    private String welcomeText;
+    private String welcomeTextEnglish;
+    private String logo;
+    private String logoEnglish;
+    private String logoLink;
+    private Integer defaultSection;
+    private Boolean official;
+    private String copyrightText;
+    private String stylesheet;
+    private String skin;
+    private Boolean showLogo;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescriptionEnglish() {
+        return descriptionEnglish;
+    }
+
+    public void setDescriptionEnglish(String descriptionEnglish) {
+        this.descriptionEnglish = descriptionEnglish;
+    }
+
+    public Boolean getOfficial() {
+        return official;
+    }
+
+    public void setOfficial(Boolean official) {
+        this.official = official;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public String getWelcomeText() {
+        return welcomeText;
+    }
+
+    public void setWelcomeText(String welcomeText) {
+        this.welcomeText = welcomeText;
+    }
+
+    public String getWelcomeTextEnglish() {
+        return welcomeTextEnglish;
+    }
+
+    public void setWelcomeTextEnglish(String welcomeTextEnglish) {
+        this.welcomeTextEnglish = welcomeTextEnglish;
+    }
+
+    public String getCopyrightText() {
+        return copyrightText;
+    }
+
+    public void setCopyrightText(String copyrightText) {
+        this.copyrightText = copyrightText;
+    }
+
+    public String getStylesheet() {
+        return stylesheet;
+    }
+
+    public void setStylesheet(String stylesheet) {
+        this.stylesheet = stylesheet;
+    }
+
+    public String getSkin() {
+        return skin;
+    }
+
+    public void setSkin(String skin) {
+        this.skin = skin;
+    }
+
+    public String getHeaderText() {
+        return headerText;
+    }
+
+    public void setHeaderText(String headerText) {
+        this.headerText = headerText;
+    }
+
+    public String getHeaderTextEnglish() {
+        return headerTextEnglish;
+    }
+
+    public void setHeaderTextEnglish(String headerTextEnglish) {
+        this.headerTextEnglish = headerTextEnglish;
+    }
+
+    public String getLogoEnglish() {
+        return logoEnglish;
+    }
+
+    public void setLogoEnglish(String logoEnglish) {
+        this.logoEnglish = logoEnglish;
+    }
+
+    public String getLogoLink() {
+        return logoLink;
+    }
+
+    public void setLogoLink(String logoLink) {
+        this.logoLink = logoLink;
+    }
+
+    public Integer getDefaultSection() {
+        return defaultSection;
+    }
+
+    public void setDefaultSection(Integer defaultSection) {
+        this.defaultSection = defaultSection;
+    }
+
+    public Boolean getShowLogo() {
+        return showLogo;
+    }
+
+    public void setShowLogo(Boolean showLogo) {
+        this.showLogo = showLogo;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitleEnglish() {
+        return titleEnglish;
+    }
+
+    public void setTitleEnglish(String titleEnglish) {
+        this.titleEnglish = titleEnglish;
+    }
+}
---
+++ erland-webapp-homepage/tags/rel_1_3/src/erland/webapp/homepage/logic/service/UrlReaderService.java
@@ -0,0 +1,49 @@
+package erland.webapp.homepage.logic.service;
+
+/*
+ * Copyright (C) 2003-2004 Erland Isaksson (erland_i@hotmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+import erland.util.ParameterValueStorageInterface;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class UrlReaderService implements ServiceInterface {
+    public String execute(ParameterValueStorageInterface parameters) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(getInputStream(parameters)));
+            StringBuffer sb = new StringBuffer(100000);
+            int character = reader.read();
+            while(character>=0) {
+                sb.append((char)character);
+                character = reader.read();
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return null;
+    }
+    protected InputStream getInputStream(ParameterValueStorageInterface parameters) throws java.io.IOException {
+        return new URL(parameters.getParameter("url")).openStream();
+    }
+}
