7cbf5ec2f69fba1e9821ebe22e1bda59505a5181
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2005-01-01 07:04:47 +0000

    Updated descriptiontags with description_en column

--- erland-webapp-dirgallery/trunk/database/common_data.sql
+++ erland-webapp-dirgallery/trunk/database/common_data.sql
@@ -8,21 +8,22 @@
 -- Dumping data for table 'descriptiontags'
 --
 
-INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','File+Size','Storlek');
-INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','File+Date/Time','Datum/Tid');
-INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+Exif+Image+Height','Höjd');
-INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+Exif+Image+Width','Bredd');
-INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+F-Number','Bländare');
-INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+Flash','Blixt');
-INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+Exposure+Time','Exponeringstid');
-INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+Focal+Length','Brännvidd');
-INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+ISO+Speed+Ratings','ISO');
-INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+Date/Time','Datum/Tid');
+INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','File+Size','Storlek','Size');
+INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','File+Date/Time','Datum/Tid','Date/Time');
+INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+Exif+Image+Height','Höjd','Height');
+INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+Exif+Image+Width','Bredd','Width');
+INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+F-Number','Bländare','Aperture');
+INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+Flash','Blixt','Flash');
+INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+Exposure+Time','Exponeringstid','Exposure');
+INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+Focal+Length','Brännvidd','Focal length');
+INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+ISO+Speed+Ratings','ISO','ISO');
+INSERT INTO descriptiontags VALUES ('common-metadatafielddescription','Exif+Date/Time','Datum/Tid','Date/Time');
 
 --
 -- Dumping data for table 'resources'
 --
 
+INSERT INTO resources VALUES ('dirgallery','resources.nativelanguage','sv');
 INSERT INTO resources VALUES ('dirgallery','resources.thumbnail.cache','D%3A%5Cusers%5Cerland%5Cthumbnails2');
 INSERT INTO resources VALUES ('dirgallery','resources.welcometext','Logga in för att uppdatera dina bildarkiv eller klicka [boldlink=här,http://erland.homeip.net/dirgallery/do/guest/home?user=erland] för att titta på mina bildarkiv. Du kan också titta på någon annans bildarkiv genom att klicka på länkarna nedan.\r\nOm du vill ha ett eget bildarkiv kan du höra av dig till [boldlink=mig,mailto:erland.i@telia.com] eller också gå vidare till [boldlink=stora bildarkivet,http://erland.homeip.net/gallery] där du kan registrera dig själv.\r\n\r\nVill du se fler bilder än vad som finns här kan du gå till [boldlink=stora bildarkivet,http://erland.homeip.net/gallery]\r\n\r\n');
 INSERT INTO resources VALUES ('dirgallery','resources.basedirectory','D%3A%5Cusers');
