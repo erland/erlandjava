0dd58ff61920be920d9ed68d1ca2e11abbdd906d
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2005-03-25 11:20:49 +0000

    Updated with new tables and columns for species support

--- erland-webapp-diary/trunk/database/diary.sql
+++ erland-webapp-diary/trunk/database/diary.sql
@@ -104,6 +104,7 @@ CREATE TABLE inventoryentries (
   link varchar(255) NOT NULL default '',
   gallery int(11) NOT NULL default '0',
   name varchar(100) NOT NULL default '',
+  species int(11) NOT NULL default '0',
   PRIMARY KEY  (id)
 ) TYPE=MyISAM;
 
@@ -201,3 +202,22 @@ CREATE TABLE useraccounts (
   PRIMARY KEY  (username)
 ) TYPE=MyISAM;
 
+CREATE TABLE species (
+  id int(11) NOT NULL auto_increment,
+  username varchar(100) NOT NULL default '',
+  name varchar(100) NOT NULL default '',
+  latinname varchar(100) NOT NULL default '',
+  type int(11) NOT NULL default '0',
+  description longtext NOT NULL,
+  image varchar(255) NOT NULL default '',
+  largeimage varchar(255) NOT NULL default '',
+  link varchar(255) NOT NULL default '',
+  gallery int(11) NOT NULL default '0',
+  PRIMARY KEY  (id)
+) TYPE=MyISAM;
+
+CREATE TABLE speciestypes (
+  id int(11) NOT NULL default '0',
+  description varchar(100) NOT NULL default '',
+  PRIMARY KEY  (id)
+) TYPE=MyISAM;
