e2bbb133b5117b812078548d564cf8b0d7000b59
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2005-04-03 18:26:27 +0000

    Added jabberNotification functionallity

--- erland-webapp-tvguide/trunk/src/erland/webapp/tvguide/act/subscription/SubscriptionPlugin.java
+++ erland-webapp-tvguide/trunk/src/erland/webapp/tvguide/act/subscription/SubscriptionPlugin.java
@@ -19,43 +19,37 @@ package erland.webapp.tvguide.act.subscription;
  *
  */
 
-import erland.webapp.common.act.BaseTaskPlugin;
-import erland.webapp.common.act.WebAppEnvironmentPlugin;
-import erland.webapp.common.WebAppEnvironmentInterface;
+import com.echomine.common.ParseException;
+import com.echomine.common.SendMessageFailedException;
+import com.echomine.jabber.*;
+import com.echomine.net.ConnectionFailedException;
+import erland.util.StringUtil;
 import erland.webapp.common.EntityInterface;
 import erland.webapp.common.QueryFilter;
 import erland.webapp.common.ServletParameterHelper;
+import erland.webapp.common.WebAppEnvironmentInterface;
+import erland.webapp.common.act.BaseTaskPlugin;
+import erland.webapp.common.act.WebAppEnvironmentPlugin;
 import erland.webapp.tvguide.entity.account.UserAccount;
 import erland.webapp.tvguide.fb.program.ProgramPB;
 import erland.webapp.tvguide.logic.program.ProgramHelper;
 import erland.webapp.usermgmt.User;
-import erland.util.StringUtil;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.struts.action.ActionServlet;
 import org.apache.struts.config.ModuleConfig;
-import org.dom4j.Document;
-import org.dom4j.io.SAXWriter;
-import org.w3c.dom.Element;
 
-import javax.servlet.ServletException;
-import javax.servlet.ServletContext;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
 import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 import javax.mail.internet.InternetAddress;
-import java.util.*;
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.InputStream;
-import java.net.URL;
-import java.text.SimpleDateFormat;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.ServletException;
+import java.net.UnknownHostException;
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class SubscriptionPlugin extends BaseTaskPlugin {
     /** Logging instance */
@@ -75,91 +69,148 @@ public class SubscriptionPlugin extends BaseTaskPlugin {
     public void init(ActionServlet actionServlet, ModuleConfig moduleConfig) throws ServletException {
         super.init(actionServlet, moduleConfig);
         me = this;
-        addTask("manager",new ManagerTask(actionServlet.getServletContext()));
+        addTask("mailnotifier",new MailNotifierTask());
+        addTask("jabbernotifier",new JabberNotifierTask());
     }
 
     public static SubscriptionPlugin getInstance() {
         return me;
     }
 
-    private class ManagerTask implements Runnable {
-        private ServletContext context;
-        public ManagerTask(ServletContext context) {
-            this.context = context;
-        }
+    private class MailNotifierTask implements Runnable {
         public void run() {
             while (true) {
-                Date notificationTime= StringUtil.asDate(getEnvironment().getConfigurableResources().getParameter("notificationtime"),null,TIME_FORMAT);
+                Date notificationTime = StringUtil.asDate(getEnvironment().getConfigurableResources().getParameter("notificationtime"), null, TIME_FORMAT);
                 Date now = new Date();
-                if(notificationTime==null) {
+                if (notificationTime == null) {
                     Calendar c = Calendar.getInstance();
-                    c.set(Calendar.HOUR_OF_DAY,0);
-                    c.set(Calendar.MINUTE,0);
-                    c.set(Calendar.SECOND,0);
-                    c.set(Calendar.MILLISECOND,0);
+                    c.set(Calendar.HOUR_OF_DAY, 0);
+                    c.set(Calendar.MINUTE, 0);
+                    c.set(Calendar.SECOND, 0);
+                    c.set(Calendar.MILLISECOND, 0);
                     notificationTime = c.getTime();
                 }
                 Calendar c = Calendar.getInstance();
                 c.setTime(notificationTime);
-                while(c.getTime().before(now)) {
-                    c.add(Calendar.DATE,1);
+                while (c.getTime().before(now)) {
+                    c.add(Calendar.DATE, 1);
                 }
                 notificationTime = c.getTime();
 
-                while(true) {
+                while (true) {
                     now = new Date();
                     try {
                         Thread.sleep(60000);
                     } catch (InterruptedException e) {
                     }
-                    if(notificationTime.before(now)) {
+                    if (notificationTime.before(now)) {
                         break;
                     }
                 }
                 String smtpServer = StringUtil.asNull(getEnvironment().getConfigurableResources().getParameter("smtp.server"));
                 String smtpFrom = StringUtil.asNull(getEnvironment().getConfigurableResources().getParameter("smtp.from"));
-                if(smtpServer!=null && smtpFrom!=null) {
+                if (smtpServer != null && smtpFrom != null) {
                     QueryFilter filter = new QueryFilter("all");
                     EntityInterface[] entities = getEnvironment().getEntityStorageFactory().getStorage("tvguide-useraccount").search(filter);
                     Session session = null;
-                    if(entities.length>0) {
+                    if (entities.length > 0) {
                         Properties props = new Properties();
-                        props.put("mail.smtp.host",smtpServer);
+                        props.put("mail.smtp.host", smtpServer);
                         session = Session.getDefaultInstance(props);
                     }
                     for (int i = 0; i < entities.length; i++) {
                         UserAccount account = (UserAccount) entities[i];
-                        if(account.getMailNotification()!=null && account.getMailNotification().booleanValue()) {
+                        if (account.getMailNotification() != null && account.getMailNotification().booleanValue()) {
                             User template = (User) getEnvironment().getEntityFactory().create("usermgmt-user");
                             template.setUsername(account.getUsername());
                             User user = (User) getEnvironment().getEntityStorageFactory().getStorage("usermgmt-user").load(template);
-                            if(user!=null && StringUtil.asNull(user.getMail())!=null) {
+                            if (user != null && StringUtil.asNull(user.getMail()) != null) {
                                 try {
                                     InternetAddress from = new InternetAddress(smtpFrom);
                                     InternetAddress to = new InternetAddress(user.getMail());
 
-                                    ProgramPB[] programsPB = ProgramHelper.getSubscribedPrograms(getEnvironment(),account.getUsername(),new Date(),null);
-                                    if(programsPB.length>0) {
+                                    ProgramPB[] programsPB = ProgramHelper.getSubscribedPrograms(getEnvironment(), account.getUsername(), new Date(), null);
+                                    if (programsPB.length > 0) {
                                         Message msg = new MimeMessage(session);
                                         msg.setFrom(from);
-                                        msg.setRecipient(Message.RecipientType.TO,to);
+                                        msg.setRecipient(Message.RecipientType.TO, to);
                                         String subject = StringUtil.asNull(getEnvironment().getConfigurableResources().getParameter("smtp.subject"));
                                         Map parameters = new HashMap();
-                                        parameters.put("date",DATE_FORMAT.format(new Date()));
-                                        msg.setSubject(ServletParameterHelper.replaceDynamicParameters(subject,parameters));
+                                        parameters.put("date", DATE_FORMAT.format(new Date()));
+                                        msg.setSubject(ServletParameterHelper.replaceDynamicParameters(subject, parameters));
                                         msg.setSentDate(new Date());
                                         msg.setText(getMessageString(programsPB));
 
-                                        LOG.info("Sending daily mail to: "+((InternetAddress)msg.getRecipients(Message.RecipientType.TO)[0]).getAddress());
+                                        LOG.info("Sending daily mail to: " + ((InternetAddress) msg.getRecipients(Message.RecipientType.TO)[0]).getAddress());
                                         Transport.send(msg);
                                     }
                                 } catch (MessagingException e) {
                                     e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
                                 }
                             }
-
                         }
                     }
+                }
+            }
+        }
+    }
+
+    private class JabberNotifierTask implements Runnable {
+        public void run() {
+            while (true) {
+                Date now = new Date();
+                try {
+                    String jabberServer = StringUtil.asNull(getEnvironment().getConfigurableResources().getParameter("jabber.server"));
+                    String jabberUser = StringUtil.asNull(getEnvironment().getConfigurableResources().getParameter("jabber.user"));
+                    String jabberPassword = StringUtil.asNull(getEnvironment().getConfigurableResources().getParameter("jabber.password"));
+                    if (jabberServer != null && jabberUser != null && jabberPassword != null) {
+                        JabberContext context = new JabberContext(jabberUser, jabberPassword, jabberServer);
+                        context.setSSL(false);
+                        Jabber jabber = new Jabber();
+                        JabberSession session = jabber.createSession(context);
+                        session.connect(jabberServer, JabberContext.DEFAULT_PORT);
+                        session.getUserService().login();
+                        QueryFilter filter = new QueryFilter("all");
+                        EntityInterface[] entities = getEnvironment().getEntityStorageFactory().getStorage("tvguide-useraccount").search(filter);
+                        for (int i = 0; i < entities.length; i++) {
+                            UserAccount account = (UserAccount) entities[i];
+                            if (StringUtil.asNull(account.getJabberId()) != null && account.getJabberNotification() != null && account.getJabberNotification().booleanValue()) {
+                                ProgramPB[] programsPB = ProgramHelper.getSubscribedPrograms(getEnvironment(), account.getUsername(), new Date(), null);
+                                Date soon = new Date();
+                                Calendar c = Calendar.getInstance();
+                                c.add(Calendar.MINUTE, 10);
+                                soon = c.getTime();
+                                for (int j = 0; j < programsPB.length; j++) {
+                                    ProgramPB program = programsPB[j];
+                                    if (program.getStart().before(soon) && program.getStart().after(now)) {
+                                        try {
+                                            session.getChatService().sendPrivateMessage(new JID(account.getJabberId()), getMessageString(program), false);
+                                        } catch (SendMessageFailedException e) {
+                                            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                                        } catch (ParseException e) {
+                                            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        Thread.sleep(5000);
+                        session.disconnect();
+                    }
+                } catch (ConnectionFailedException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                } catch (SendMessageFailedException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                } catch (JabberMessageException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                } catch (InterruptedException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+                try {
+                    Thread.sleep(60000*10);
+                } catch (InterruptedException e) {
                 }
             }
         }
@@ -168,11 +219,17 @@ public class SubscriptionPlugin extends BaseTaskPlugin {
         StringBuffer sb = new StringBuffer();
         for (int i = 0; i < programs.length; i++) {
             ProgramPB program = programs[i];
-            sb.append(program.getStartTimeDisplay()+" - "+program.getStopTimeDisplay()+" "+program.getName()+" ("+program.getChannelName()+")");
+            sb.append("\n"+program.getStartTimeDisplay()+" - "+program.getStopTimeDisplay()+" "+program.getName()+" ("+program.getChannelName()+")");
             sb.append("\n"+StringUtil.wordWrap(program.getDescription(),80));
             sb.append("\n");
-            sb.append("\n");
         }
+        return sb.toString();
+    }
+    private String getMessageString(ProgramPB program) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(" \n"+program.getStartTimeDisplay()+" - "+program.getStopTimeDisplay()+" "+program.getName()+" ("+program.getChannelName()+")");
+        sb.append("\n"+StringUtil.wordWrap(program.getDescription(),40));
+        sb.append("\n");
         return sb.toString();
     }
 }
