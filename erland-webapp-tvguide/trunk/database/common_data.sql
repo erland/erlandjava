5ab8e81834ec44b8cbb1cfa51fa8d9e55331667c
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2005-04-03 11:07:42 +0000

    Corrected error

--- erland-webapp-tvguide/trunk/database/common_data.sql
+++ erland-webapp-tvguide/trunk/database/common_data.sql
@@ -12,9 +12,9 @@ INSERT INTO resources VALUES ('tvguide','resources.welcometext','Login to update
 INSERT INTO resources VALUES ('tvguide','resources.welcometext_sv','Logga in för att uppdatera din TV-Guide, för att registrera din egen TV-Guide måste du kontakta [boldlinksamewindow=mig,mailto:erland_i@hotmail.com]\r\n\r\n');
 INSERT INTO resources VALUES ('tvguide','resources.nativelanguage','sv');
 INSERT INTO resources VALUES ('tvguide','resources.helppath','http://erland.homeip.net/help/do/guest/home?application=tvguide[context,&chapter={context}]');
-INSERT INTO resources VALUES ('tvguide','resources.nativelanguage','sv')
-INSERT INTO resources VALUES ('tvguide','resources.smtp.server','')
-INSERT INTO resources VALUES ('tvguide','resources.smtp.from','')
-INSERT INTO resources VALUES ('tvguide','resources.smtp.subject','TV-program {date}')
+INSERT INTO resources VALUES ('tvguide','resources.nativelanguage','sv');
+INSERT INTO resources VALUES ('tvguide','resources.smtp.server','');
+INSERT INTO resources VALUES ('tvguide','resources.smtp.from','');
+INSERT INTO resources VALUES ('tvguide','resources.smtp.subject','TV-program {date}');
 INSERT INTO resources VALUES ('tvguide','resources.notificationtime','06:00');
 
