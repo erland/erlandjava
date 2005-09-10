466edfc0400369ae7c9aaea6dbc6258dfefd437e
erland <erland@c7d3dfe0-b00b-7b2c-0e97-889c7e347ecd>
2005-09-10 16:15:03 +0000

    Added handling of statistic data

--- erland-webapp-stock/trunk/src/erland/webapp/stocks/act/account/GetAccountValueAction.java
+++ erland-webapp-stock/trunk/src/erland/webapp/stocks/act/account/GetAccountValueAction.java
@@ -22,26 +22,19 @@ import erland.webapp.common.act.WebAppEnvironmentPlugin;
 import erland.webapp.stocks.bl.entity.StockAccount;
 import erland.webapp.stocks.bl.entity.StockAccountValue;
 import erland.webapp.stocks.bl.service.StockAccountManager;
-import erland.webapp.stocks.bl.logic.account.StockAccountStockEntryListInterface;
 import erland.webapp.stocks.fb.account.AccountDiagramFB;
 import erland.webapp.stocks.fb.account.AccountValuePB;
-import erland.webapp.stocks.fb.account.AccountStatisticPB;
-import erland.webapp.stocks.fb.stock.SelectFB;
-import erland.webapp.diagram.DateValueSerieInterface;
-import erland.webapp.diagram.DateValueDiagramHelper;
+import erland.webapp.stocks.fb.account.AccountYearStatisticPB;
+import erland.webapp.stocks.fb.account.AccountStockStatisticPB;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Iterator;
-import java.util.Vector;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionForm;
-import org.apache.commons.beanutils.PropertyUtils;
 
 public class GetAccountValueAction extends Action {
 
@@ -51,37 +44,64 @@ public class GetAccountValueAction extends Action {
         StockAccount account = accountManager.getAccount(httpServletRequest.getRemoteUser());
         if(fb.getStartDate()!=null && fb.getEndDate()!=null) {
             StockAccountValue value;
-            double purchaseValue = 0;
             if(fb.getBroker()!=null && fb.getStock()!=null) {
                 value = account.getStockValue(fb.getBroker(),fb.getStock(),fb.getEndDate());
-                purchaseValue = account.getPurchaseValue(fb.getBroker(),fb.getStock(),fb.getEndDate());
             }else if(fb.getBroker()!=null && fb.getStock()==null) {
                 value = account.getStockValue(fb.getBroker(),fb.getEndDate());
-                purchaseValue = account.getPurchaseValue(fb.getBroker(),fb.getEndDate());
             }else {
                 value = account.getStockValue(fb.getEndDate());
-                purchaseValue = account.getPurchaseValue(fb.getEndDate());
             }
             AccountValuePB pb = new AccountValuePB();
             pb.setDate(fb.getEndDate());
             pb.setValue(new Double(value.getValue()));
-            pb.setPurchaseValue(new Double(purchaseValue));
+            pb.setPurchaseValue(new Double(value.getPurchaseValue()));
+            pb.setIncreasedValue(new Double(value.getIncreasedValue()));
             if(value.getNoOfStocks()>0) {
                 pb.setNoOfStocks(new Double(value.getNoOfStocks()));
             }
             if(value.getRate()>0) {
                 pb.setCurrentRate(new Double(value.getRate()));
             }
-            pb.setTotalStatistic(new Double((value.getValue()-purchaseValue)*100.0/purchaseValue));
-            int[] years = value.getStatisticYears();
-            if(years.length>0) {
-                AccountStatisticPB[] yearStats=new AccountStatisticPB[years.length];
-                for (int i = 0; i < years.length; i++) {
-                    yearStats[i] = new AccountStatisticPB();
-                    yearStats[i].setYear(new Integer(years[i]));
-                    yearStats[i].setValue(new Double(value.getStatistic(years[i])));
-                }
+            pb.setTotalStatistic(new Double(value.getTotalStatistics()));
+            pb.setTotalStatisticThisYear(new Double(value.getTotalStatisticsThisYear()));
+            int[] years = value.getYearStatistic();
+            AccountYearStatisticPB[] yearStats=new AccountYearStatisticPB[years.length];
+            for (int i = 0; i < years.length; i++) {
+                yearStats[i] = new AccountYearStatisticPB();
+                yearStats[i].setYear(new Integer(years[i]));
+                yearStats[i].setPercent(new Double(value.getYearStatisticPercent(years[i])));
+                yearStats[i].setValue(new Double(value.getYearStatisticValue(years[i])));
+                yearStats[i].setPurchase(new Double(value.getYearStatisticPurchase(years[i])));
             }
+            Arrays.sort(yearStats,new Comparator() {
+                public int compare(Object o1, Object o2) {
+                    return ((AccountYearStatisticPB)o1).getYear().compareTo(((AccountYearStatisticPB)o2).getYear());
+                }
+            });
+            pb.setStatisticsPerYear(yearStats);
+
+            String[] stockIds = value.getStockStatistic();
+            AccountStockStatisticPB[] stockStats=new AccountStockStatisticPB[stockIds.length];
+            for (int i = 0; i < stockIds.length; i++) {
+                stockStats[i] = new AccountStockStatisticPB();
+                stockStats[i].setStock(value.getStockStatisticStockName(stockIds[i]));
+                stockStats[i].setBroker(value.getStockStatisticBroker(stockIds[i]));
+                stockStats[i].setPercent(new Double(value.getStockStatisticPercent(stockIds[i])));
+                stockStats[i].setPercentThisYear(new Double(value.getStockStatisticPercentThisYear(stockIds[i])));
+                stockStats[i].setValue(new Double(value.getStockStatisticValue(stockIds[i])));
+                stockStats[i].setIncreasedValue(new Double(value.getStockStatisticIncreasedValue(stockIds[i])));
+                stockStats[i].setPurchase(new Double(value.getStockStatisticPurchase(stockIds[i])));
+            }
+            Arrays.sort(stockStats,new Comparator() {
+                public int compare(Object o1, Object o2) {
+                    int comp = ((AccountStockStatisticPB)o1).getBroker().compareTo(((AccountStockStatisticPB)o2).getBroker());
+                    if(comp!=0) {
+                        return comp;
+                    }
+                    return -((AccountStockStatisticPB)o1).getPercentThisYear().compareTo(((AccountStockStatisticPB)o2).getPercentThisYear());
+                }
+            });
+            pb.setStatisticsPerStock(stockStats);
             httpServletRequest.setAttribute("accountValuePB",pb);
         }
         return actionMapping.findForward("success");
